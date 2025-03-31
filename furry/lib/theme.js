7b933316b6d20a6f795adc17e929afb564c1a839
jrmph13 <jhames.martin.dev@gmail.com>
2025-03-31 04:59:50 +0000

    chore(deps): refactor view templates

--- furry/lib/theme.js
+++ furry/lib/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_3bdac846f0ea474e94246e05653dffc5 = () => {
-  return '2025-03-16T06:57:31Z';
+const node_8a68ce0a96f94c43b08510bfac3d4e5a = () => {
+  return '2025-03-31T04:59:50Z';
 };
