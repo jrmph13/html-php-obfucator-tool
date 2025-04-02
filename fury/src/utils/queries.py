ae72cf4a8dea09ea4e653a6736e8c5497189c039
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-02 23:12:05 +0000

    feat(auth): enhance session store

--- fury/src/utils/queries.py
+++ fury/src/utils/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8d873ab2f076452bb4a1c98b108effba = () => {
-  return '2025-03-07T18:33:11Z';
+const node_25893a06ea6e4ab89bc1cbfacbf57b7d = () => {
+  return '2025-04-02T23:12:05Z';
 };
