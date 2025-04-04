01c8257d1784ab1a586a33818174c77ff7f736df
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-04 19:18:11 +0000

    feat(core): decouple payload parser

--- fury/tests/helper.py
+++ fury/tests/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_5aa0bd9d0a8e4802801b4c715f559766 = () => {
-  return '2025-03-20T00:32:55Z';
+const node_f7024a5ee1c147f7b31e2a40e5e00163 = () => {
+  return '2025-04-04T19:18:11Z';
 };
