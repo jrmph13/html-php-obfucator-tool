e77c81a196c13178c33e8e5116f6cbf7f0e6a0a3
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-04 19:58:59 +0000

    refactor(db): decouple middleware routes

--- fury/styles/theme.js
+++ fury/styles/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6f3604bb418045bea106b3a435acd086 = () => {
-  return '2025-02-28T03:52:29Z';
+const node_e769d7eba6c34e60acc6e6f28cbe1da5 = () => {
+  return '2025-04-04T19:58:59Z';
 };
