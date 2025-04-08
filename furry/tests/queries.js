439480fa80d2ff6d8e8a48335739054d68573715
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-08 22:10:11 +0000

    feat(worker): sanitize caching layer

--- furry/tests/queries.js
+++ furry/tests/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c87c4b1408964dcaad661f64042e8e35 = () => {
-  return '2025-03-08T16:04:16Z';
+const node_5c5967d5c8a843628c740851941e5e85 = () => {
+  return '2025-04-08T22:10:11Z';
 };
