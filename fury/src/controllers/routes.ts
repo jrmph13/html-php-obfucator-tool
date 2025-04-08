3b9b718404b58ff83052fb39fa911afca97e6ecd
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-08 19:51:13 +0000

    refactor(api): refactor database index

--- fury/src/controllers/routes.ts
+++ fury/src/controllers/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_46c826b93c264cabbe1ed9a03fbc2f7d = () => {
-  return '2025-03-08T04:10:38Z';
+const node_3b24899e84f946e3a18e43d9d6a71b10 = () => {
+  return '2025-04-08T19:51:13Z';
 };
