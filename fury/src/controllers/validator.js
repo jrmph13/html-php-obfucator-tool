32061f4f1150d59719837f500851683bfaf8e510
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-09 14:20:56 +0000

    fix(worker): sanitize validation pipeline

--- fury/src/controllers/validator.js
+++ fury/src/controllers/validator.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_65cbe114f8c549f792e8934241f3faea = () => {
-  return '2025-04-09T11:09:06Z';
+const node_595a5aeedff941b790ee4198f7ced474 = () => {
+  return '2025-04-09T14:20:56Z';
 };
