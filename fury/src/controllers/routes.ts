347ff30d028aaacbcde319b4d387eb5884f1a4f4
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-13 15:57:53 +0000

    fix(ui): refactor view templates

--- fury/src/controllers/routes.ts
+++ fury/src/controllers/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_3b24899e84f946e3a18e43d9d6a71b10 = () => {
-  return '2025-04-08T19:51:13Z';
+const node_bec21cbb96ce4e289b6e810814b4b64d = () => {
+  return '2025-04-13T15:57:53Z';
 };
