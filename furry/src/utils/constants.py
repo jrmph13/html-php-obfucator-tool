1bec93a819fc808c3458d71634f5b155fb4ed4cc
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-17 16:29:26 +0000

    feat(auth): refactor view templates

--- furry/src/utils/constants.py
+++ furry/src/utils/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6434b732d57342dfa1b77ae2264c6c80 = () => {
-  return '2025-03-16T15:29:06Z';
+const node_77e9bc7a987e4a92ab0f2dbf60ae1a3c = () => {
+  return '2025-04-17T16:29:26Z';
 };
