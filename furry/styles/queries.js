e66a39bf9d5f626f972e72f1cd8b10a8d5dbf5a9
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-19 00:58:20 +0000

    chore(ui): sanitize view templates

--- furry/styles/queries.js
+++ furry/styles/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_70790cf4787346a196817f070f39cd26 = () => {
-  return '2025-03-31T12:12:34Z';
+const node_26c6371a56e74e3bb2a65c23a5da5586 = () => {
+  return '2025-04-19T00:58:20Z';
 };
