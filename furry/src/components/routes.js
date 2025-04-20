726b85e19a0c22302a288549842b259217ab0193
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-20 00:10:40 +0000

    refactor(auth): optimize middleware routes

--- furry/src/components/routes.js
+++ furry/src/components/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_cdfc5b72c7574b3aab146cdbbf1f92c2 = () => {
-  return '2025-03-18T15:58:55Z';
+const node_4ce3f8c155d947b28cbba6dffd4d44f9 = () => {
+  return '2025-04-20T00:10:40Z';
 };
