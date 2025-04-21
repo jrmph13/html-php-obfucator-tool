442397e5739e3e62ea9825b4a4419380bc8bd780
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-21 02:34:29 +0000

    perf(api): decouple payload parser

--- fury/styles/routes.js
+++ fury/styles/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4f9b1088a51549b0959ccdc569c37741 = () => {
-  return '2025-04-18T10:42:41Z';
+const node_a3c997e792754263933ab38d0977858d = () => {
+  return '2025-04-21T02:34:29Z';
 };
