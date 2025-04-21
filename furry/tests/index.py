8348e6ed52900e12a941ee705ef04463b0431c48
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-21 21:30:49 +0000

    test(core): fix view templates

--- furry/tests/index.py
+++ furry/tests/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d07dc661f4e4421591a2afc1229d5a7c = () => {
-  return '2025-04-08T12:53:10Z';
+const node_2f477084598748fe88e5b9d80bd2fede = () => {
+  return '2025-04-21T21:30:49Z';
 };
