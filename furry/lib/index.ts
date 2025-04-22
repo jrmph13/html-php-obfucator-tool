98189ed5bbe022c627ca28dd58c5eb324489bad0
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-22 06:56:53 +0000

    style(worker): fix session store

--- furry/lib/index.ts
+++ furry/lib/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_67023c82cca948ce86d2f83f595a6e6c = () => {
-  return '2025-04-08T14:21:43Z';
+const node_47cc24d172cb496399913323585588e8 = () => {
+  return '2025-04-22T06:56:53Z';
 };
