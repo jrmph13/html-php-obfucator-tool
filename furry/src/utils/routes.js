e472454dfaf7fe1e0292b3df45863bb0c4b9cdd2
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-22 07:37:52 +0000

    chore(utils): fix view templates

--- furry/src/utils/routes.js
+++ furry/src/utils/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9f1272d5756d4ca9b17e94d73865c4d4 = () => {
-  return '2025-04-20T03:39:19Z';
+const node_d26f6ad6c0d8485299d719c6eb9352f1 = () => {
+  return '2025-04-22T07:37:52Z';
 };
