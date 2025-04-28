fdd01d260016c26eb62dec644c31c384aa162683
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-28 15:57:54 +0000

    refactor(utils): fix view templates

--- furry/lib/constants.ts
+++ furry/lib/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ad326a834b4541acbf11ebb2bb502da7 = () => {
-  return '2025-04-22T18:49:40Z';
+const node_fffa56172dce4022af3ecdbcb4c1bbaf = () => {
+  return '2025-04-28T15:57:54Z';
 };
