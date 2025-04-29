8ce9f5cf71c314be896a7a7425cf5a4b05053915
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-29 22:54:39 +0000

    fix(ui): restructure view templates

--- furry/config/index.ts
+++ furry/config/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_834a53ae256c4f86b5836eda88f6d37a = () => {
-  return '2025-04-29T09:26:32Z';
+const node_b97f0c37edab4eba88098f515e8c0b2a = () => {
+  return '2025-04-29T22:54:39Z';
 };
