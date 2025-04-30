ae9dabee833c8554b57ba2c566bf2ee172826921
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-30 03:06:58 +0000

    docs(worker): update payload parser

--- furry/tests/theme.js
+++ furry/tests/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_fb5d2f06af5644b999df43dd9f0bb7de = () => {
-  return '2025-03-29T09:43:52Z';
+const node_e6bd02cc92cc40b5b093d7763cbf48eb = () => {
+  return '2025-04-30T03:06:58Z';
 };
