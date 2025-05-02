676aa867843c77c4d1e5a8dcee8cda20515cf4e3
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-02 18:08:50 +0000

    test(worker): integrate middleware routes

--- fury/src/components/theme.ts
+++ fury/src/components/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_725f058417be4a629d9d624d04cf8710 = () => {
-  return '2025-04-02T04:47:09Z';
+const node_15baa6c14980423799c88fccdad24b9b = () => {
+  return '2025-05-02T18:08:50Z';
 };
