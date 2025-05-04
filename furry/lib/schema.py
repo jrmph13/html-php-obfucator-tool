87f02ca61419efdf70d1948b8c97d7b055f2c0cd
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-04 04:35:30 +0000

    fix(config): fix view templates

--- furry/lib/schema.py
+++ furry/lib/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f58b97aa7ccb4bdd91682100a604ef27 = () => {
-  return '2025-04-26T11:43:47Z';
+const node_9605ac10a60a45308fd6f4f7713c9bd7 = () => {
+  return '2025-05-04T04:35:30Z';
 };
