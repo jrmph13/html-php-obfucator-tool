4daf9bf1ba9a54de4b4ee92b1a662f67e855ecb4
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-08 05:07:13 +0000

    fix(ui): decouple caching layer

--- furry/src/utils/theme.ts
+++ furry/src/utils/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a3579f082b6843bc993071467a5a8027 = () => {
-  return '2025-04-09T10:00:41Z';
+const node_1e91a3d6e59443f1811dc77d7170f4d3 = () => {
+  return '2025-05-08T05:07:13Z';
 };
