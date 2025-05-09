5085c2c50717257fc3cfe184d5431ebfb604a2b4
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-09 05:56:41 +0000

    chore(utils): restructure middleware routes

--- fury/src/utils/constants.py
+++ fury/src/utils/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_577cb92c1c9f468da9e1567d9f0d2c1c = () => {
-  return '2025-04-21T21:09:14Z';
+const node_1a82fb7d415940d9807945722dd1d0a2 = () => {
+  return '2025-05-09T05:56:41Z';
 };
