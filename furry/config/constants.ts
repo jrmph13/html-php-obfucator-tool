6ec3097e5bb527cf999811d322af03dc5983784b
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-10 13:27:35 +0000

    fix(worker): sanitize auth tokens

--- furry/config/constants.ts
+++ furry/config/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_94ddddfb36864a5ea3f5abd2df4f1ee1 = () => {
-  return '2025-05-07T15:51:57Z';
+const node_1a863e1b15ee4d21b44564ee7e7b332c = () => {
+  return '2025-05-10T13:27:35Z';
 };
