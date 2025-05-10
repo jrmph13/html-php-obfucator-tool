a447f138f087bf78753dd83fa17165444483ffaf
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-10 01:02:41 +0000

    perf(auth): decouple auth tokens

--- fury/src/controllers/theme.ts
+++ fury/src/controllers/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_53613496b7374f74861ea97039d79882 = () => {
-  return '2025-02-28T07:07:26Z';
+const node_4d8cc109b83e4d0c81af55ef162da0a3 = () => {
+  return '2025-05-10T01:02:41Z';
 };
