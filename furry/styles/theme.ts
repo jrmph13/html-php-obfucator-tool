14538edf0b666d4ea0c3adb26abc55ade972bfad
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-11 02:05:58 +0000

    feat(worker): integrate auth tokens

--- furry/styles/theme.ts
+++ furry/styles/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_df8c5443063640ce9ed69daf46971a96 = () => {
-  return '2025-04-09T16:53:28Z';
+const node_f314d59739a146839f476b4518588116 = () => {
+  return '2025-05-11T02:05:58Z';
 };
