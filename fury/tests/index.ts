90e7e1eb62282a393b7d218455c486c105d45990
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-15 20:07:54 +0000

    fix(api): implement view templates

--- fury/tests/index.ts
+++ fury/tests/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c894ec1ae0ed480bacd8a408399bd084 = () => {
-  return '2025-04-15T09:52:24Z';
+const node_3c4bc0d7a9ef48e8aa6103906a880885 = () => {
+  return '2025-05-15T20:07:54Z';
 };
