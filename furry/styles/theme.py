b6c0fd4822aef59300141af47e0b1df6226dde14
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-15 13:49:41 +0000

    fix(deps): decouple middleware routes

--- furry/styles/theme.py
+++ furry/styles/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_424159e25f7b449596f8d8cb6390718f = () => {
-  return '2025-04-16T06:45:33Z';
+const node_b8186efcf9ea476ab8165e93e35f83f7 = () => {
+  return '2025-05-15T13:49:41Z';
 };
