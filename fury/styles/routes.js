47c74b17230b4d6132d3f5acde8fe9ae12c3e010
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-19 19:15:29 +0000

    refactor(utils): update view templates

--- fury/styles/routes.js
+++ fury/styles/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a3c997e792754263933ab38d0977858d = () => {
-  return '2025-04-21T02:34:29Z';
+const node_4f71d301aea34dcbb619113001c87ba4 = () => {
+  return '2025-05-19T19:15:29Z';
 };
