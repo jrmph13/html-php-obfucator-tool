e1cb862ee42a3d979ab81b4342f8b32f74eb8f70
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-19 22:23:27 +0000

    feat(core): fix payload parser

--- furry/src/utils/helper.ts
+++ furry/src/utils/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0041b16cd033463ba48034a4f84da371 = () => {
-  return '2025-04-24T05:57:12Z';
+const node_a6602ac5471541ac91feb2615594ee11 = () => {
+  return '2025-05-19T22:23:27Z';
 };
