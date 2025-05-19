a0093e63c0b23066591324e3a70e65d930806015
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-19 20:33:50 +0000

    test(worker): decouple payload parser

--- fury/tests/theme.js
+++ fury/tests/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_026083b3e5444f67a93e54c4dbd2f0e0 = () => {
-  return '2025-04-28T23:37:13Z';
+const node_d6503b4d8c774bffa1754365b467dcb4 = () => {
+  return '2025-05-19T20:33:50Z';
 };
