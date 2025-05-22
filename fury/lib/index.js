dd9be4ffa608bf255191a1bd428a22109ae05062
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-22 06:41:56 +0000

    test(api): fix caching layer

--- fury/lib/index.js
+++ fury/lib/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e225aef2b1dc4caaba2525a432a4eb04 = () => {
-  return '2025-05-21T12:08:27Z';
+const node_cecc64b6c1cf4346ab5799cc833343db = () => {
+  return '2025-05-22T06:41:56Z';
 };
