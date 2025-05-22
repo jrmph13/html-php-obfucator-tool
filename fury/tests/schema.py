2b1bc455ea1833b5f3181589da87d997990a290d
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-22 06:12:05 +0000

    style(worker): optimize view templates

--- fury/tests/schema.py
+++ fury/tests/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_218f52323428425a9657219bda4e698d = () => {
-  return '2025-04-26T07:15:02Z';
+const node_752faf42f5c249d0b56885aa84c92055 = () => {
+  return '2025-05-22T06:12:05Z';
 };
