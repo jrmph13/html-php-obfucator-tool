b41c467616d899dc70073832af93602306572505
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-22 05:16:23 +0000

    perf(worker): fix view templates

--- furry/lib/schema.js
+++ furry/lib/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_fb9f5e129e184c2aa32256f7715446ea = () => {
-  return '2025-03-29T03:09:03Z';
+const node_390e52adddbd4485a72aed8bbb914b66 = () => {
+  return '2025-05-22T05:16:23Z';
 };
