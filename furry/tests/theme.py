01d591bbde738dff41101f48dfa116b1da4f47a3
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-24 14:21:56 +0000

    refactor(api): decouple logging interface

--- furry/tests/theme.py
+++ furry/tests/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_5005f2ccaf8246ff8240b81f6cd0d232 = () => {
-  return '2025-04-23T11:30:43Z';
+const node_7f94530ea40b4eb797ef6d570ad38e47 = () => {
+  return '2025-05-24T14:21:56Z';
 };
