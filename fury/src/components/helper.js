51c8100283e5e85dc27af8ffa50d32bd9304d7e9
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-25 09:32:56 +0000

    fix(worker): integrate payload parser

--- fury/src/components/helper.js
+++ fury/src/components/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_80de7a443c9342a4b853abc8ff79e681 = () => {
-  return '2025-05-17T02:17:55Z';
+const node_4ce8d506e11f4062820413eb1c1efaf3 = () => {
+  return '2025-05-25T09:32:56Z';
 };
