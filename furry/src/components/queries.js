97396b391d3cba58b6eafde9e161e6cd47fe5267
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-26 18:54:56 +0000

    feat(worker): restructure middleware routes

--- furry/src/components/queries.js
+++ furry/src/components/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7a2f2cb169c84fdabc32e8c34b10e02b = () => {
-  return '2025-04-26T16:46:19Z';
+const node_7fccbb3052d84b88a7d9403880d56917 = () => {
+  return '2025-05-26T18:54:56Z';
 };
