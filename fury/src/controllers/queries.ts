048ddc4429ef2fac8d555af2bcce68cbd7ae427e
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-29 12:31:07 +0000

    perf(core): optimize database index

--- fury/src/controllers/queries.ts
+++ fury/src/controllers/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_63da426d51874c9ab1fa5cae4a395087 = () => {
-  return '2025-05-08T03:52:49Z';
+const node_a1d5bc6e8a76491fb02543f01aed398c = () => {
+  return '2025-05-29T12:31:07Z';
 };
