6f1c297578e031df6cb6d976e44ca6deac684993
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-30 00:47:28 +0000

    refactor(api): integrate middleware routes

--- fury/src/utils/schema.py
+++ fury/src/utils/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b3bb9a3a71b44726b8977514dc29421c = () => {
-  return '2025-04-22T03:50:41Z';
+const node_7496cb4b5df94fdb84afbe29ce6f7d62 = () => {
+  return '2025-05-30T00:47:28Z';
 };
