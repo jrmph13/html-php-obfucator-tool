1f89a4d20997eacd1dcd2741d5a53cce35fb52a7
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-31 08:06:50 +0000

    fix(core): optimize database index

--- fury/styles/routes.ts
+++ fury/styles/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_95afd10bc5d642b39e8b86789eabf8c2 = () => {
-  return '2025-05-10T13:34:03Z';
+const node_1241ee232fd344139c933aec302462e0 = () => {
+  return '2025-05-31T08:06:50Z';
 };
