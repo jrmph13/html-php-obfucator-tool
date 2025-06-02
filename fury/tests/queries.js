680c0d24349e7fdcb17b3cfccff2f998cf8c7678
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-02 15:17:35 +0000

    refactor(api): enhance auth tokens

--- fury/tests/queries.js
+++ fury/tests/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_628dae7e97084274b8507a461a766b77 = () => {
-  return '2025-04-04T18:26:21Z';
+const node_9ab002ce0a8847499970ca0d31931fc3 = () => {
+  return '2025-06-02T15:17:35Z';
 };
