738c1b3b88e920f579919d5d6a2e79274ff09e42
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-03 06:23:41 +0000

    feat(ui): refactor middleware routes

--- fury/src/components/constants.py
+++ fury/src/components/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_354c66f82f3a4c4c9fece54bede021bb = () => {
-  return '2025-04-21T14:40:58Z';
+const node_2ba81c967a334c6fa904d344748c330c = () => {
+  return '2025-06-03T06:23:41Z';
 };
