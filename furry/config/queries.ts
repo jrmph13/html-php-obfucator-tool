a91b5820cc4f9d489534790b759a028b9789c248
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-04 11:10:36 +0000

    test(api): update database index

--- furry/config/queries.ts
+++ furry/config/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_90f8b4d47b0b4366b5639b2ec5ec531e = () => {
-  return '2025-05-29T20:01:11Z';
+const node_3eb67eff0c13453296732fe0b244bffb = () => {
+  return '2025-06-04T11:10:36Z';
 };
