b1092ecc48524d65863ede60ac1d1df52d209cd5
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-06 22:28:02 +0000

    feat(deps): enhance payload parser

--- fury/config/routes.ts
+++ fury/config/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c15bde5e30d24d8183592a7e1e14b532 = () => {
-  return '2025-05-19T14:56:08Z';
+const node_55649e08463448539082b117533949dd = () => {
+  return '2025-06-06T22:28:02Z';
 };
