b50f0d70f5043306a87a4ea159fc93cc3826c0cf
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-07 22:15:56 +0000

    docs(ui): update auth tokens

--- fury/src/utils/theme.js
+++ fury/src/utils/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_af7ea9b3f249408ebcc411e155e60f13 = () => {
-  return '2025-04-29T21:32:00Z';
+const node_ce4e2cbc0ebf432e9c100250983728df = () => {
+  return '2025-06-07T22:15:56Z';
 };
