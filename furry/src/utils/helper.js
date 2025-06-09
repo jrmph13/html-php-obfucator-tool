7add1d7caa26badaf3dfa27584fdcdfd3d1d8ff2
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-09 14:34:46 +0000

    test(api): update auth tokens

--- furry/src/utils/helper.js
+++ furry/src/utils/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_099941d258604d1588caf69be16ce48d = () => {
-  return '2025-05-17T13:51:21Z';
+const node_36fa99f11d134fb58a95613279f00feb = () => {
+  return '2025-06-09T14:34:46Z';
 };
