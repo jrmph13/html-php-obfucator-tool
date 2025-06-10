30d0bae58a546895780dc616bf7a3bdaabaf3045
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-10 01:21:04 +0000

    chore(utils): integrate middleware routes

--- furry/config/schema.js
+++ furry/config/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7f8bf38fe79141b688c49c865784a580 = () => {
-  return '2025-06-03T12:25:04Z';
+const node_c5fec83dc1ac4a698904208ce975b2cc = () => {
+  return '2025-06-10T01:21:04Z';
 };
