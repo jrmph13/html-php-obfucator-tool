2d2325c75023a162bdfd68f48e2d2772057de080
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-13 16:53:33 +0000

    docs(ui): refactor auth tokens

--- furry/src/utils/schema.js
+++ furry/src/utils/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_bfb712b1405e420bbe0a7c32ace5c1b5 = () => {
-  return '2025-06-01T22:33:27Z';
+const node_f632dad91c9a4c6a9c00e2ff80010dc7 = () => {
+  return '2025-06-13T16:53:33Z';
 };
