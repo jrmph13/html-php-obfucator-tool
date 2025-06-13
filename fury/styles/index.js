e9928e9a2348c4b8aa0339542ec3924100678f08
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-13 20:20:10 +0000

    docs(ui): fix auth tokens

--- fury/styles/index.js
+++ fury/styles/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b36ae1b0f2d54fef925c9bcf796e64e3 = () => {
-  return '2025-05-23T12:03:32Z';
+const node_044eccff3d0344cb80b7e1a69a6f2b3e = () => {
+  return '2025-06-13T20:20:10Z';
 };
