3482b567f179245d99ff238846bf82a7478050fa
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-13 14:14:10 +0000

    fix(utils): decouple auth tokens

--- fury/lib/routes.ts
+++ fury/lib/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7801b7dc577f458189c0cdf1400fad08 = () => {
-  return '2025-06-13T03:41:51Z';
+const node_f380e636be8f41c48c896bb8e532bac5 = () => {
+  return '2025-06-13T14:14:10Z';
 };
