9b240c9aee92999b72a7d8e26e2e80facee4c907
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-17 11:50:36 +0000

    test(core): enhance middleware routes

--- furry/tests/routes.ts
+++ furry/tests/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_021bc4904aee4ee088375f36357157fb = () => {
-  return '2025-06-07T18:19:52Z';
+const node_263abaad7afd463d845dd7ded213ddc4 = () => {
+  return '2025-06-17T11:50:36Z';
 };
