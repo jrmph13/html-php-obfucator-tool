1bda50b53f9481d16354c418a8c4540d09e877c5
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-19 14:47:46 +0000

    feat(api): sanitize auth tokens

--- fury/src/components/routes.py
+++ fury/src/components/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d36a0b65968249feb4fc0184759636fa = () => {
-  return '2025-05-31T14:49:43Z';
+const node_98706270d87b4375befac405baffea01 = () => {
+  return '2025-06-19T14:47:46Z';
 };
