dfacab4bdbfc61d2433e1eccb087172c368af836
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-21 00:13:06 +0000

    perf(config): restructure auth tokens

--- fury/lib/helper.js
+++ fury/lib/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_316b074a014b4fca95ec25cff5c1498a = () => {
-  return '2025-04-05T04:50:39Z';
+const node_45e90c9b70744c1ba45d77aea8557185 = () => {
+  return '2025-06-21T00:13:06Z';
 };
