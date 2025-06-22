b02905054b5bce4a55890e2cb7e50c426c4c43e4
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-22 12:14:34 +0000

    feat(utils): refactor auth tokens

--- fury/lib/routes.ts
+++ fury/lib/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f380e636be8f41c48c896bb8e532bac5 = () => {
-  return '2025-06-13T14:14:10Z';
+const node_543b52f9bbd74104ae975cb78aaba989 = () => {
+  return '2025-06-22T12:14:34Z';
 };
