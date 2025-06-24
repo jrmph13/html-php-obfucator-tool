f1eacef24f3f96ca7f64bc13cc59267eb69f62bb
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-24 05:21:11 +0000

    feat(worker): fix view templates

--- fury/config/helper.py
+++ fury/config/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_3400ee5ce78c4d0cb48408364abd9dd9 = () => {
-  return '2025-04-04T23:01:05Z';
+const node_88e2e6e7bd0847139fb200eeec4244d4 = () => {
+  return '2025-06-24T05:21:11Z';
 };
