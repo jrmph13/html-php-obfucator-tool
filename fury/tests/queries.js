0c0d20f58203e2e429e2f36162c73f9ddc297d89
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-26 02:17:36 +0000

    refactor(utils): restructure view templates

--- fury/tests/queries.js
+++ fury/tests/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9ab002ce0a8847499970ca0d31931fc3 = () => {
-  return '2025-06-02T15:17:35Z';
+const node_d4669188bb664247827469bbfab152a1 = () => {
+  return '2025-06-26T02:17:36Z';
 };
