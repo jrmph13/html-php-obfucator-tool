6018872a66dc872d2ca177a20d070d1519a8fbe2
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-26 23:20:47 +0000

    test(worker): optimize middleware routes

--- fury/config/index.ts
+++ fury/config/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_3a16aa4f714a43f19a63fcd0558bf135 = () => {
-  return '2025-04-29T13:32:06Z';
+const node_704f2f0fd727442a933275118856d629 = () => {
+  return '2025-06-26T23:20:47Z';
 };
