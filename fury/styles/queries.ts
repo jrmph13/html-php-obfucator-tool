61b2e9e08b6916dee489e5161a7c43d194aa57a5
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-30 01:45:33 +0000

    style(api): decouple caching layer

--- fury/styles/queries.ts
+++ fury/styles/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_1f724676707f4b5f91ffa6bd3fe555bd = () => {
-  return '2025-06-30T02:01:48Z';
+const node_1cfdfb46024e46c8b35f84090bf51887 = () => {
+  return '2025-06-30T01:45:33Z';
 };
