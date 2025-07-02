4ffc2eb150dcc9668f311a712157474871ee389b
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-02 03:23:36 +0000

    fix(api): decouple validation pipeline

--- furry/tests/schema.py
+++ furry/tests/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_85dae07569f34e6d97c133ab3ab8b42a = () => {
-  return '2025-04-08T04:46:13Z';
+const node_2b9cea3571db4b9e9067faab7036ddd6 = () => {
+  return '2025-07-02T03:23:36Z';
 };
