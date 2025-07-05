a7801dc5efd92f0625cba0b2abff1534185a7cef
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-05 08:49:50 +0000

    test(auth): update database index

--- fury/config/queries.js
+++ fury/config/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_16768bcc10654daf94d07ff3bdf5094f = () => {
-  return '2025-06-22T01:38:47Z';
+const node_92e32515f5644ea28b3ca761bc6b61a7 = () => {
+  return '2025-07-05T08:49:50Z';
 };
