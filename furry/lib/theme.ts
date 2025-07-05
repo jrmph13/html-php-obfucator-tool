bb5db2f3b77f1cb543dc6bd12599dfaeb9e88537
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-05 20:56:37 +0000

    perf(utils): restructure caching layer

--- furry/lib/theme.ts
+++ furry/lib/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_99ae974c2aea4b6d8b077316d78caa7d = () => {
-  return '2025-06-13T02:12:05Z';
+const node_6f56c0960e2648e490f2ad8408ea98b9 = () => {
+  return '2025-07-05T20:56:37Z';
 };
