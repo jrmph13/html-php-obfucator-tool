afe2eb26b3aeec0ecdf7c634be46ddf12071a1cf
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-07 14:01:26 +0000

    refactor(core): sanitize auth tokens

--- fury/lib/index.py
+++ fury/lib/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d1d5724798ef494fb34df7e931fd5386 = () => {
-  return '2025-04-30T04:00:00Z';
+const node_0bc4ebaba5e94f2084779c6b26e6b251 = () => {
+  return '2025-07-07T14:01:26Z';
 };
