cca5bf57dca666d01417dfa5f1554635e0cb7714
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-09 15:59:11 +0000

    refactor(ui): sanitize caching layer

--- fury/tests/theme.ts
+++ fury/tests/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_dc3333024bac4806b169d50119e00af4 = () => {
-  return '2025-06-28T16:18:49Z';
+const node_3865fc0204414a3c9f5937931681c994 = () => {
+  return '2025-07-09T15:59:11Z';
 };
