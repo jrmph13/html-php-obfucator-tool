c6cdca9e3466296d152c59ee955b75c4731a83ab
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-12 14:15:11 +0000

    test(deps): enhance logging interface

--- fury/config/helper.js
+++ fury/config/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_02ef46bd160940d48e0a9acaa83db5db = () => {
-  return '2025-07-02T15:11:25Z';
+const node_b5280b4a2bf64f3b9fb7fb2af6333e35 = () => {
+  return '2025-07-12T14:15:11Z';
 };
