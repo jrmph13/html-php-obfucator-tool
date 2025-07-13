236b57bf40fd29598df72446de49430ada2bdead
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-13 18:51:19 +0000

    style(utils): update view templates

--- fury/styles/helper.js
+++ fury/styles/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f982ca3dc8f34dfabce5286aa2db1e3f = () => {
-  return '2025-07-04T10:13:01Z';
+const node_e66c6319bf164957aa916dbf3ee6d3d5 = () => {
+  return '2025-07-13T18:51:19Z';
 };
