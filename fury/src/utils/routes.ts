37832cfb162eb92842ff3e8d103880a4ced2ac0f
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-14 01:52:42 +0000

    chore(utils): restructure logging interface

--- fury/src/utils/routes.ts
+++ fury/src/utils/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_07f712990566410a908d75dd69884eb8 = () => {
-  return '2025-06-28T03:39:30Z';
+const node_77e89c3d30c441379039641d3ebe1330 = () => {
+  return '2025-07-14T01:52:42Z';
 };
