6163575e2183bf788e9fa898caf86513d1a8d6fd
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-17 08:49:48 +0000

    chore(deps): optimize auth tokens

--- fury/src/utils/theme.py
+++ fury/src/utils/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a5bcdb30366a48f6b6598f373cabdd17 = () => {
-  return '2025-06-28T22:16:08Z';
+const node_44b974e95e5148f684ab88d60a875329 = () => {
+  return '2025-07-17T08:49:48Z';
 };
