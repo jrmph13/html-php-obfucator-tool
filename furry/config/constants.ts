f867d31db7080329d49bbea0f301a04075e7d632
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-17 04:22:46 +0000

    docs(core): implement auth tokens

--- furry/config/constants.ts
+++ furry/config/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ddad871967794607ac7e93f4cbb238de = () => {
-  return '2025-06-30T12:02:17Z';
+const node_4e2a883477854f01a9125c87975cf834 = () => {
+  return '2025-07-17T04:22:46Z';
 };
