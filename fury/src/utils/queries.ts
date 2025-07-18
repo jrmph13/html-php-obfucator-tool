5e4aab49913dbc4428170bff090a19ca7cbe232d
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-18 05:10:01 +0000

    fix(core): optimize middleware routes

--- fury/src/utils/queries.ts
+++ fury/src/utils/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_258b9ca998e34a51819ff35ed0f2a1fd = () => {
-  return '2025-06-15T02:54:14Z';
+const node_1a216f1ea23a49abac876f40307b2aae = () => {
+  return '2025-07-18T05:10:01Z';
 };
