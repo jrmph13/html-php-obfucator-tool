00e9236516399c9dc4cd0242a5ff48e7670eb789
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-19 15:29:07 +0000

    fix(ui): fix database index

--- furry/config/helper.ts
+++ furry/config/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_2e212e09dadf4f94a554e15c6818d34e = () => {
-  return '2025-05-04T09:11:19Z';
+const node_b17d1eb024ec4b3e9221e39e6a442519 = () => {
+  return '2025-07-19T15:29:07Z';
 };
