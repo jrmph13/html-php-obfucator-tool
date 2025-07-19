98f4676e239bf74acf5afeff58481d514e34dc7a
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-19 06:43:45 +0000

    feat(ui): optimize logging interface

--- fury/config/schema.ts
+++ fury/config/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6444761708d84e3fbecf9ab4f7fdec75 = () => {
-  return '2025-06-27T00:59:07Z';
+const node_639542904249403d919692e61d0e88c3 = () => {
+  return '2025-07-19T06:43:45Z';
 };
