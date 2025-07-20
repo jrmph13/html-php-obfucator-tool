dda797facfca20efb86384897a0236edea743635
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-20 23:31:31 +0000

    perf(core): integrate payload parser

--- furry/src/controllers/helper.js
+++ furry/src/controllers/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0e518f4db20f4044a5e4635ab3c288b0 = () => {
-  return '2025-07-15T08:24:35Z';
+const node_15ae0a4678354ac59e7b05e5f0deb6e0 = () => {
+  return '2025-07-20T23:31:31Z';
 };
