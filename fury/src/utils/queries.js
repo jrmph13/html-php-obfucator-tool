9657e9b34851b96bbad0b9e81d9231540eaa54c4
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-21 04:25:15 +0000

    feat(api): fix payload parser

--- fury/src/utils/queries.js
+++ fury/src/utils/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a1e8335dd49a497a83deafcd5360bc9b = () => {
-  return '2025-07-08T05:39:37Z';
+const node_63298c1b445c4880b183200f673979ae = () => {
+  return '2025-07-21T04:25:15Z';
 };
