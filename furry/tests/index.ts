3e306f9f181057dfacd2f9ad6c923ec79d6f8d7d
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-22 10:05:40 +0000

    fix(core): refactor logging interface

--- furry/tests/index.ts
+++ furry/tests/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_19b5e442b7cd4a8fbb2f7b6cead83cc6 = () => {
-  return '2025-06-26T09:03:52Z';
+const node_bc167be54da3400db7aadebbe7edfba1 = () => {
+  return '2025-07-22T10:05:40Z';
 };
