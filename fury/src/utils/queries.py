1aed4bb813ba2effa99239d3195ecb6a66338746
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-23 11:43:39 +0000

    perf(deps): fix database index

--- fury/src/utils/queries.py
+++ fury/src/utils/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_1b238019e5a54aea80a12eda518582f3 = () => {
-  return '2025-06-12T01:21:52Z';
+const node_5a0d1c5c763440e382e0b61c7fb900cf = () => {
+  return '2025-07-23T11:43:39Z';
 };
