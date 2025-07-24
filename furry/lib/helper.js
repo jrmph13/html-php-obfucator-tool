dd187e2993174f8c8299d115fc1ba2ceed86adfe
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-24 20:16:38 +0000

    perf(worker): decouple payload parser

--- furry/lib/helper.js
+++ furry/lib/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b7e8cdd109994698bd47908e8b8d0aec = () => {
-  return '2025-07-02T21:33:43Z';
+const node_552afc5895674960ab6223eeff47272f = () => {
+  return '2025-07-24T20:16:38Z';
 };
