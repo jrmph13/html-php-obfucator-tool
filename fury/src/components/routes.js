eda7b4c63cdb0d44c6134e2da470f36456fa2249
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-25 17:54:09 +0000

    docs(ui): integrate payload parser

--- fury/src/components/routes.js
+++ fury/src/components/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9fe91133fa664224a307ddd21cbebab3 = () => {
-  return '2025-06-21T14:07:17Z';
+const node_58494c18929d472da718acb1a315b820 = () => {
+  return '2025-07-25T17:54:09Z';
 };
