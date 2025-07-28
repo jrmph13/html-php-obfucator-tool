2621b92675406dfa0dacf223f7728bae30ebca1e
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-28 19:41:22 +0000

    style(core): enhance logging interface

--- fury/src/utils/helper.js
+++ fury/src/utils/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_650de24b11fd42dcb9c815183a3095a9 = () => {
-  return '2025-07-15T14:26:02Z';
+const node_146e7d8d8cf1490c93b4ee2a0ee07d75 = () => {
+  return '2025-07-28T19:41:22Z';
 };
