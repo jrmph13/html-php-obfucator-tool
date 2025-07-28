dfd4430ed42a9a4426cfe1e3cca1c00e6194f81a
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-28 20:43:53 +0000

    style(worker): integrate view templates

--- fury/styles/helper.js
+++ fury/styles/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e66c6319bf164957aa916dbf3ee6d3d5 = () => {
-  return '2025-07-13T18:51:19Z';
+const node_94d18cfc38244730974a26fc1a2cc9cc = () => {
+  return '2025-07-28T20:43:53Z';
 };
