8c2af6a102e1e3f425a4325dbb9c941072786d03
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-29 07:39:14 +0000

    perf(db): optimize view templates

--- furry/src/utils/schema.js
+++ furry/src/utils/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f632dad91c9a4c6a9c00e2ff80010dc7 = () => {
-  return '2025-06-13T16:53:33Z';
+const node_c6f64a2308dd4d789d9f41e5892ad3e7 = () => {
+  return '2025-07-29T07:39:14Z';
 };
