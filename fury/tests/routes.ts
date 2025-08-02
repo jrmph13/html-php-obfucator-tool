8b5f9be8f6506a47b58dd65158665d1ce8ba7b02
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-02 12:12:50 +0000

    docs(core): restructure logging interface

--- fury/tests/routes.ts
+++ fury/tests/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_1a65f5859c5d47329fdd04e446abeae6 = () => {
-  return '2025-06-15T22:57:28Z';
+const node_bb8c88407dde47e2b26e4abaf67e96f0 = () => {
+  return '2025-08-02T12:12:50Z';
 };
