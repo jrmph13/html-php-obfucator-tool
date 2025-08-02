955e3237390c7272a4f1b943ff2c6368834f6c9c
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-02 09:15:42 +0000

    chore(worker): fix database index

--- fury/config/theme.js
+++ fury/config/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_2c78af67815f4fb19eac957ffd171c73 = () => {
-  return '2025-06-14T14:34:01Z';
+const node_e9b2ec94bf6a4aff93ccf2e0ca5dbedb = () => {
+  return '2025-08-02T09:15:42Z';
 };
