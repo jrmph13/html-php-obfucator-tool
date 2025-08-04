083898ed3d80e45cfd00a0be29135efcbcfac074
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-04 09:35:52 +0000

    test(api): sanitize logging interface

--- fury/src/utils/theme.py
+++ fury/src/utils/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_44b974e95e5148f684ab88d60a875329 = () => {
-  return '2025-07-17T08:49:48Z';
+const node_185f0252cd5c47edb64742171f8b0dd9 = () => {
+  return '2025-08-04T09:35:52Z';
 };
