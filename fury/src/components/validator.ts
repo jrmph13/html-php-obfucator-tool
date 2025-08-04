d9a34bbff593c541d9b03e681d4feb46a894e0e5
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-04 11:44:03 +0000

    chore(auth): fix auth tokens

--- fury/src/components/validator.ts
+++ fury/src/components/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d78ffa0160ea4a4eb1e07cae1e95478d = () => {
-  return '2025-07-31T04:05:31Z';
+const node_a943d31b1f09473d8bf46cdac59b64f2 = () => {
+  return '2025-08-04T11:44:03Z';
 };
