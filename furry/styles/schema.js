0f66934c6246c6ed68f3438d2ce5d14a550cb530
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-04 06:15:56 +0000

    test(auth): decouple auth tokens

--- furry/styles/schema.js
+++ furry/styles/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_88337549b5a64c0ba55d2a0488e7ef81 = () => {
-  return '2025-06-12T09:47:03Z';
+const node_5b89b18710a34041828018d54e5b5744 = () => {
+  return '2025-08-04T06:15:56Z';
 };
