32f05c438951a0197f042da61a4fac28c843a771
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-05 18:29:47 +0000

    fix(ui): restructure middleware routes

--- fury/src/controllers/schema.py
+++ fury/src/controllers/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_727e5c5cf3a942639d1540aaab7f2063 = () => {
-  return '2025-07-27T04:58:11Z';
+const node_9df1358dce1a437dbf96aea1ed35b6ce = () => {
+  return '2025-08-05T18:29:47Z';
 };
