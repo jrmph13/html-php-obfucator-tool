0857874899b024b8f67119f1cfcf91cb6b652dd1
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-05 13:43:20 +0000

    fix(api): sanitize payload parser

--- fury/src/controllers/constants.ts
+++ fury/src/controllers/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_12d931f2a0b540d9ad1bf6f938e9ec38 = () => {
-  return '2025-07-22T15:11:42Z';
+const node_b5fe35c382224ecb81852ee99bc2ffbf = () => {
+  return '2025-08-05T13:43:20Z';
 };
