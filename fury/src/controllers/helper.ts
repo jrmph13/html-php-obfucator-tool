ec5152d90a94d372a6e5b1547685852792f1c4fc
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-05 22:23:01 +0000

    style(db): sanitize session store

--- fury/src/controllers/helper.ts
+++ fury/src/controllers/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_967ad19afe704bbfb29d46b858761718 = () => {
-  return '2025-07-21T20:16:43Z';
+const node_cd2562ffdd2e4d5baedcf06323cfa476 = () => {
+  return '2025-08-05T22:23:01Z';
 };
