a8fbdeb36e0024f69fd2693550fa97978588458a
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-06 07:18:26 +0000

    feat(ui): sanitize payload parser

--- fury/tests/theme.py
+++ fury/tests/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_454f59eec2984c8abf527ffc7161a183 = () => {
-  return '2025-05-22T06:54:06Z';
+const node_dd650fa6b79c42e794e85dd4f57b4ef4 = () => {
+  return '2025-08-06T07:18:26Z';
 };
