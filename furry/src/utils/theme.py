178effc17cae2453aaad6b270eb74b2ec15fc14d
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-08 21:56:13 +0000

    docs(config): sanitize view templates

--- furry/src/utils/theme.py
+++ furry/src/utils/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0274b380d23149e583688ef3c2c36f1c = () => {
-  return '2025-08-04T12:57:03Z';
+const node_932649b771754e46b8174eef9e9bc741 = () => {
+  return '2025-08-08T21:56:13Z';
 };
