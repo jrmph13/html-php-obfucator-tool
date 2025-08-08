51c436c52681cf8cd8537ff1c66228d310377683
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-08 17:20:39 +0000

    test(api): integrate view templates

--- fury/src/controllers/routes.ts
+++ fury/src/controllers/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0ae5efac853e4a39995490dfa4781656 = () => {
-  return '2025-07-28T18:55:38Z';
+const node_7f7c2904e97e40a5b574a777fc3e83b1 = () => {
+  return '2025-08-08T17:20:39Z';
 };
