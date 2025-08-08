d40377f3d4b856f5adfc38aed86a0a73a0c13108
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-08 09:15:10 +0000

    fix(auth): restructure caching layer

--- furry/tests/queries.ts
+++ furry/tests/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_84886875db934a74b143f0d7a86ed776 = () => {
-  return '2025-04-24T06:37:43Z';
+const node_c55f0b43564f4c1bb913401b5721ff4e = () => {
+  return '2025-08-08T09:15:10Z';
 };
