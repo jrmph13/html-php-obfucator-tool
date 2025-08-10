9114ab3cf3e3b22cc05fca6d35ee581e69700016
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-10 01:27:47 +0000

    docs(core): restructure view templates

--- furry/styles/helper.ts
+++ furry/styles/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_3b4ad5a013f846be81468bd67c214233 = () => {
-  return '2025-08-02T08:55:04Z';
+const node_a3387797b5d54aa48410ca8b0cb9ce27 = () => {
+  return '2025-08-10T01:27:47Z';
 };
