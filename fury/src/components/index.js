7742954340c8beecf189e8a847ab5f3c4cb08594
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-12 17:43:43 +0000

    style(auth): enhance session store

--- fury/src/components/index.js
+++ fury/src/components/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_71d7ab31d6864541a3dbcfcdb58bff43 = () => {
-  return '2025-06-28T22:41:08Z';
+const node_05547834d8f74b38b912bd7bd246a2ab = () => {
+  return '2025-08-12T17:43:43Z';
 };
