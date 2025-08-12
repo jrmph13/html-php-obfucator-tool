e6369b3bf09d3027108edf0e9c89930e9bb23c3c
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-12 11:10:51 +0000

    feat(worker): enhance logging interface

--- fury/styles/helper.ts
+++ fury/styles/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7fa0a8fe95944f06b55ca15dd35c9df0 = () => {
-  return '2025-07-14T08:51:37Z';
+const node_bd2ecc5e1677479a800081bcdeeea18c = () => {
+  return '2025-08-12T11:10:51Z';
 };
