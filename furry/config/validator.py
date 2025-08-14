bf2bb8178c2cb1933ce425b4635e383b841eab63
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-14 07:12:15 +0000

    style(worker): decouple auth tokens

--- furry/config/validator.py
+++ furry/config/validator.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7d16bb7c4e884d7db3f00c7a96b5d554 = () => {
-  return '2025-07-22T20:46:24Z';
+const node_8d62af44a407472d85bf02695ea4866b = () => {
+  return '2025-08-14T07:12:15Z';
 };
