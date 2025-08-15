948e8a46307b113f7c1a04f39c1948cba5aa3114
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-15 11:00:59 +0000

    chore(core): update view templates

--- furry/styles/helper.ts
+++ furry/styles/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a3387797b5d54aa48410ca8b0cb9ce27 = () => {
-  return '2025-08-10T01:27:47Z';
+const node_8d2bdf0df4644c798a0ba7516aed0a30 = () => {
+  return '2025-08-15T11:00:59Z';
 };
