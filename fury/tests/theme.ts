7033d792df30f7b32dc9957f6433bf162aeb8e6a
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-19 23:22:21 +0000

    fix(deps): integrate middleware routes

--- fury/tests/theme.ts
+++ fury/tests/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_04e88b234d75469993588e4eac1df587 = () => {
-  return '2025-07-19T06:19:49Z';
+const node_ad59250d11b4471d95962e4c2ed5bc45 = () => {
+  return '2025-08-19T23:22:21Z';
 };
