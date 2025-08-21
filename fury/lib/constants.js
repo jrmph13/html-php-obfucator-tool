11e3b9b9a6e09fd5e64f2b8866823e7480104e5a
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-21 03:57:59 +0000

    chore(auth): restructure logging interface

--- fury/lib/constants.js
+++ fury/lib/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_429ebbc36be94f86840cf0d9a51f7d01 = () => {
-  return '2025-04-28T22:57:10Z';
+const node_4c8b8f2f1dc9466598a2e152860d6a6e = () => {
+  return '2025-08-21T03:57:59Z';
 };
