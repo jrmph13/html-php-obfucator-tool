e328fddb90a59663accbf31e551cdf2dd3def850
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-21 09:09:37 +0000

    fix(worker): optimize database index

--- fury/src/controllers/helper.py
+++ fury/src/controllers/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f5eb7f072c1a4c3094fa46440f5a2b80 = () => {
-  return '2025-07-19T05:17:26Z';
+const node_8dd4a629d3a04de8b36acf592ae66d00 = () => {
+  return '2025-08-21T09:09:37Z';
 };
