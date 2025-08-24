d5ab6441f2137703639272b1db5cf36878c0328b
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-24 18:43:32 +0000

    refactor(ui): optimize payload parser

--- furry/src/components/helper.js
+++ furry/src/components/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_55ec3b2dc27d451ebdba6181367e59bd = () => {
-  return '2025-05-23T23:10:49Z';
+const node_320613818ce34745a70ed12f6e9d1515 = () => {
+  return '2025-08-24T18:43:32Z';
 };
