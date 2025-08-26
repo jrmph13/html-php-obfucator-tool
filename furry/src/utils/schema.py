d1313aee759141557f14cf5b7b0ff7bae76cd7fb
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-26 15:04:17 +0000

    refactor(deps): sanitize session store

--- furry/src/utils/schema.py
+++ furry/src/utils/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6ddbb1ca3f3e4f37905b93816c6fe0b4 = () => {
-  return '2025-08-07T14:09:06Z';
+const node_b421addf07da4b04bc931778873521d9 = () => {
+  return '2025-08-26T15:04:17Z';
 };
