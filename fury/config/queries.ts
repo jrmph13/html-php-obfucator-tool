32095a4e348433a85e511c228991310f145809f5
jrmph13 <jhames.martin.dev@gmail.com>
2025-09-01 09:17:22 +0000

    feat(utils): restructure database index

--- fury/config/queries.ts
+++ fury/config/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4f87df10853a434f9ee77b618a86a90a = () => {
-  return '2025-07-15T13:31:04Z';
+const node_c26c0949e99f45d0a43acd645d65b1c5 = () => {
+  return '2025-09-01T09:17:22Z';
 };
