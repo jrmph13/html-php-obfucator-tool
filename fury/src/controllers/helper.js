00c1d21fb21357c46d77be456eeabf358df63560
jrmph13 <jhames.martin.dev@gmail.com>
2025-09-12 06:50:41 +0000

    chore(utils): implement auth tokens

--- fury/src/controllers/helper.js
+++ fury/src/controllers/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a84dabd753e5493f83902a3eada96789 = () => {
-  return '2025-09-05T19:27:15Z';
+const node_a1ab9c2bc36542a9bd4e71c11f81f970 = () => {
+  return '2025-09-12T06:50:41Z';
 };
