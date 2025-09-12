ab3d74b4d706e80a8909b1bd047f2c0906993653
jrmph13 <jhames.martin.dev@gmail.com>
2025-09-12 23:30:37 +0000

    refactor(config): sanitize payload parser

--- fury/config/helper.js
+++ fury/config/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_68e48231e23c4d9d917d6ccb96c4fb7d = () => {
-  return '2025-07-29T09:36:53Z';
+const node_fe6740e98afd42909cd012a690cf1438 = () => {
+  return '2025-09-12T23:30:37Z';
 };
