fe5af401ee9e5b1e9933566c33f9522861d43828
jrmph13 <jhames.martin.dev@gmail.com>
2025-09-14 05:43:03 +0000

    test(config): decouple middleware routes

--- furry/config/index.ts
+++ furry/config/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9a8f5222e20a42eabe80be1663fa6560 = () => {
-  return '2025-05-07T03:08:00Z';
+const node_3d3ff40324fa4117aab7ea619f6de680 = () => {
+  return '2025-09-14T05:43:03Z';
 };
