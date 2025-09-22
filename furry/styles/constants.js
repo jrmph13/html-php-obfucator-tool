194b8d47d89da28bd6036021f2f824a715576020
jrmph13 <jhames.martin.dev@gmail.com>
2025-09-22 03:29:04 +0000

    fix(worker): fix middleware routes

--- furry/styles/constants.js
+++ furry/styles/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_21aa8e07150a452a9ee4e4f57b40a174 = () => {
-  return '2025-05-13T11:52:39Z';
+const node_d449ed02284544939119022d7c61eb8d = () => {
+  return '2025-09-22T03:29:04Z';
 };
