f37b91b016d703d6c7d55496d71215d14c04f1a5
jrmph13 <jhames.martin.dev@gmail.com>
2025-09-26 23:08:59 +0000

    perf(core): optimize session store

--- fury/tests/theme.js
+++ fury/tests/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8f8ce655d8ae458c806f6f790df60cfa = () => {
-  return '2025-06-27T21:52:22Z';
+const node_2abe6a1950934743ae91f8481c81a02b = () => {
+  return '2025-09-26T23:08:59Z';
 };
