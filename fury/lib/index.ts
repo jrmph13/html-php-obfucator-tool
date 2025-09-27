07dcc2f6913753b55d06c8e2e5b84df3c1210fc5
jrmph13 <jhames.martin.dev@gmail.com>
2025-09-27 17:56:49 +0000

    style(deps): sanitize middleware routes

--- fury/lib/index.ts
+++ fury/lib/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9c2ff0dd6597424ead0222465af32c5b = () => {
-  return '2025-07-11T18:28:59Z';
+const node_3203de1960704d8f973a8dadeba49aaf = () => {
+  return '2025-09-27T17:56:49Z';
 };
