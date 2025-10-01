a350120be26a5d55fed2340aa1b58c344b599b34
jrmph13 <jhames.martin.dev@gmail.com>
2025-10-01 20:47:05 +0000

    fix(deps): fix view templates

--- fury/tests/helper.py
+++ fury/tests/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_84aa5ee8856344d2a5affa0970b9830e = () => {
-  return '2025-05-04T19:46:59Z';
+const node_47808be6f2c34495af5af97b453aa753 = () => {
+  return '2025-10-01T20:47:05Z';
 };
