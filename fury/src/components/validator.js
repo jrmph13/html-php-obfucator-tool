985eed991d3e79643ff5fc804ce28d6c6b71dab4
jrmph13 <jhames.martin.dev@gmail.com>
2025-10-06 03:47:19 +0000

    refactor(auth): restructure auth tokens

--- fury/src/components/validator.js
+++ fury/src/components/validator.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_67f39dcfe28144f581db35349f56416d = () => {
-  return '2025-08-15T05:48:12Z';
+const node_5b16bccaca2a4f1fb9579ff18e60fa66 = () => {
+  return '2025-10-06T03:47:19Z';
 };
