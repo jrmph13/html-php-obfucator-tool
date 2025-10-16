4188100ada5df472144fbee84037e689494b7d43
jrmph13 <jhames.martin.dev@gmail.com>
2025-10-16 09:38:02 +0000

    test(worker): fix view templates

--- furry/config/helper.ts
+++ furry/config/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0cd0d88c95f94a289876dfc2ae4f6818 = () => {
-  return '2025-09-05T20:03:30Z';
+const node_2b5981dfafb1421d993cecf0f6310e5a = () => {
+  return '2025-10-16T09:38:02Z';
 };
