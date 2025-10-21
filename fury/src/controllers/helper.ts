2ae138042caa058e5b86271adb32d8b976cb5b9b
jrmph13 <jhames.martin.dev@gmail.com>
2025-10-21 03:02:44 +0000

    style(worker): enhance session store

--- fury/src/controllers/helper.ts
+++ fury/src/controllers/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_cd2562ffdd2e4d5baedcf06323cfa476 = () => {
-  return '2025-08-05T22:23:01Z';
+const node_56e0708f64134df1aa1c0b2b90f1bd53 = () => {
+  return '2025-10-21T03:02:44Z';
 };
