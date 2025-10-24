4fed465ac16c9e5ab63fa615e16b093637d6b2d3
jrmph13 <jhames.martin.dev@gmail.com>
2025-10-24 18:22:32 +0000

    perf(ui): decouple view templates

--- fury/src/components/routes.py
+++ fury/src/components/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_acdc73704a924900904adf293750e5ab = () => {
-  return '2025-07-05T15:22:59Z';
+const node_55b1a8ae74a34261a896a274489ff94f = () => {
+  return '2025-10-24T18:22:32Z';
 };
