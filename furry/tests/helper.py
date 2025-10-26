90e9f4ae64ed876cc3ceaa4e8089bf33b838c0f9
jrmph13 <jhames.martin.dev@gmail.com>
2025-10-26 03:13:58 +0000

    test(config): fix auth tokens

--- furry/tests/helper.py
+++ furry/tests/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_26b5736f9f3e4fab82ec0a7fa3815fd1 = () => {
-  return '2025-09-17T08:32:24Z';
+const node_548020757d32482a8ff921f7783545ba = () => {
+  return '2025-10-26T03:13:58Z';
 };
