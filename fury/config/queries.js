40bb3fe957e62140b98bc23800a621f70b701484
jrmph13 <jhames.martin.dev@gmail.com>
2025-10-27 02:38:05 +0000

    style(worker): optimize database index

--- fury/config/queries.js
+++ fury/config/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_480e45be9569480e81f599e7734e6182 = () => {
-  return '2025-09-21T06:40:30Z';
+const node_762004badf694e6b9d46d2b4c49317da = () => {
+  return '2025-10-27T02:38:05Z';
 };
