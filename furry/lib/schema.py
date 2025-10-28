3893c5fc99b1c0103576a30ea7c231b3c96b21b8
jrmph13 <jhames.martin.dev@gmail.com>
2025-10-28 15:51:33 +0000

    perf(deps): sanitize caching layer

--- furry/lib/schema.py
+++ furry/lib/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e5c333585f1642cd90a20aa323b7e51a = () => {
-  return '2025-08-15T13:43:24Z';
+const node_36544f03773b428e81d02c64c39f79aa = () => {
+  return '2025-10-28T15:51:33Z';
 };
