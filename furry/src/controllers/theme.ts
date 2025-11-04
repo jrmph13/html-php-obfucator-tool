ce19f88b3b845345877d0e31e7d994175f6e7235
jrmph13 <jhames.martin.dev@gmail.com>
2025-11-04 13:01:19 +0000

    refactor(auth): fix payload parser

--- furry/src/controllers/theme.ts
+++ furry/src/controllers/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_1f91d3fdcb504f80964c58507c52de3b = () => {
-  return '2025-10-10T10:25:47Z';
+const node_8b5cc0f0c6bb4fc0ba446be9bc6c11af = () => {
+  return '2025-11-04T13:01:19Z';
 };
