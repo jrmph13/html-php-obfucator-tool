e625cbd22a1030d1d8cef8c22b4bde260732697a
jrmph13 <jhames.martin.dev@gmail.com>
2025-11-07 11:58:59 +0000

    style(core): fix database index

--- fury/lib/helper.js
+++ fury/lib/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4681e2d514cb4cea928feeea44d85d1c = () => {
-  return '2025-07-15T07:49:01Z';
+const node_04b47f0b4224485cb45089c23096e6e5 = () => {
+  return '2025-11-07T11:58:59Z';
 };
