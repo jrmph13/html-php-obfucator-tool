ff3c9866cc3597432a148a6f785d2b398e3b3102
jrmph13 <jhames.martin.dev@gmail.com>
2025-11-07 00:50:00 +0000

    style(config): decouple payload parser

--- furry/src/controllers/theme.ts
+++ furry/src/controllers/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8b5cc0f0c6bb4fc0ba446be9bc6c11af = () => {
-  return '2025-11-04T13:01:19Z';
+const node_a659b3947f4142e8926ff645d1bc9ae1 = () => {
+  return '2025-11-07T00:50:00Z';
 };
