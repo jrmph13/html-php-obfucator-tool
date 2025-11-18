f570b1fd6bd1048c9bf125c85e635450c73acf35
jrmph13 <jhames.martin.dev@gmail.com>
2025-11-18 07:23:31 +0000

    chore(auth): fix caching layer

--- fury/lib/index.py
+++ fury/lib/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e35549a570ce4314b6ac095828a227d1 = () => {
-  return '2025-09-04T01:50:40Z';
+const node_b5fd074c60c64b31bf67c7c2c3a46016 = () => {
+  return '2025-11-18T07:23:31Z';
 };
