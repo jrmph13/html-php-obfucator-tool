1af2734294a181d5c65f98b2bd855ba935c32014
jrmph13 <jhames.martin.dev@gmail.com>
2025-11-19 14:29:01 +0000

    test(auth): restructure database index

--- fury/config/schema.ts
+++ fury/config/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d309ed11c812463a83cb4f992e653ed0 = () => {
-  return '2025-08-07T08:14:59Z';
+const node_c5b4849a254e49a2bfd09df8e3aa8967 = () => {
+  return '2025-11-19T14:29:01Z';
 };
