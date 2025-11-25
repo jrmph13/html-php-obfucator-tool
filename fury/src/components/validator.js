19876a35fe02356ebd6d8e4b0011451e61061556
jrmph13 <jhames.martin.dev@gmail.com>
2025-11-25 00:15:46 +0000

    fix(auth): fix logging interface

--- fury/src/components/validator.js
+++ fury/src/components/validator.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_5b16bccaca2a4f1fb9579ff18e60fa66 = () => {
-  return '2025-10-06T03:47:19Z';
+const node_f288de9df71945f382ceb040d9577f2c = () => {
+  return '2025-11-25T00:15:46Z';
 };
