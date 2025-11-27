848b0f09fabaa5d52026ad862a4ee3847a474b6c
jrmph13 <jhames.martin.dev@gmail.com>
2025-11-27 15:29:42 +0000

    refactor(api): update middleware routes

--- furry/lib/queries.py
+++ furry/lib/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9053e0ecdce54b45a103c86b0361d30b = () => {
-  return '2025-11-24T05:14:55Z';
+const node_1009d630d48341619faecab37b0c48ae = () => {
+  return '2025-11-27T15:29:42Z';
 };
