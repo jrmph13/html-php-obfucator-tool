a1dd33af43da0d41b83a8c3078f745a593e42874
jrmph13 <jhames.martin.dev@gmail.com>
2025-12-09 13:42:54 +0000

    style(auth): restructure view templates

--- furry/src/controllers/queries.ts
+++ furry/src/controllers/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_32c787a7e64c4ce4a2cab25e070107f9 = () => {
-  return '2025-07-31T14:01:05Z';
+const node_d1920af74e0544caa165fb562e3cd752 = () => {
+  return '2025-12-09T13:42:54Z';
 };
