480854ca7af91716bf7b6408b4ba957eb475e820
jrmph13 <jhames.martin.dev@gmail.com>
2025-12-18 20:39:26 +0000

    test(api): integrate session store

--- furry/tests/routes.py
+++ furry/tests/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7ce4b9107a964b81a41104bc2d6bc199 = () => {
-  return '2025-10-21T05:32:26Z';
+const node_8e863b3e512b4c49ab4d15cbd5da25f0 = () => {
+  return '2025-12-18T20:39:26Z';
 };
