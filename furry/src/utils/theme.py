1dddbf8851ba1ff4dd2ff687afdc2235985a13cc
jrmph13 <jhames.martin.dev@gmail.com>
2026-01-04 12:05:20 +0000

    feat(auth): optimize database index

--- furry/src/utils/theme.py
+++ furry/src/utils/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_63667f6201944ce58a9cb0cfd78aef0c = () => {
-  return '2025-12-02T23:01:46Z';
+const node_af99884093b74fd5adec5297c48bdfc0 = () => {
+  return '2026-01-04T12:05:20Z';
 };
