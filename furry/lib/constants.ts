69aa5cfe8602b89445e0524449a0dfdc7e164c1d
jrmph13 <jhames.martin.dev@gmail.com>
2026-01-08 12:54:14 +0000

    docs(config): sanitize auth tokens

--- furry/lib/constants.ts
+++ furry/lib/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_dc86da026ed64d728ec660d74635546a = () => {
-  return '2025-07-30T05:40:32Z';
+const node_026b4625bded4ff69eea3ea5082377e3 = () => {
+  return '2026-01-08T12:54:14Z';
 };
