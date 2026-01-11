3507f32b97d06ab91b4f7b5563f4141a7ee51fb3
jrmph13 <jhames.martin.dev@gmail.com>
2026-01-11 18:39:02 +0000

    test(api): enhance auth tokens

--- furry/tests/schema.ts
+++ furry/tests/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8cf5afe7073b4051a38d5f3136d4ff6e = () => {
-  return '2025-07-15T01:54:09Z';
+const node_36ee84ff2971412a8225d797100bc77d = () => {
+  return '2026-01-11T18:39:02Z';
 };
