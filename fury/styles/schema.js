37e83d0946cde80a4ca5fcbaf47f2bec4758eec7
jrmph13 <jhames.martin.dev@gmail.com>
2026-01-20 17:40:03 +0000

    feat(ui): sanitize caching layer

--- fury/styles/schema.js
+++ fury/styles/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4f0b490958124bd2bab337f749df1afa = () => {
-  return '2025-08-11T07:54:28Z';
+const node_2ca4c58c9f8747d7972d4220e5adaf7c = () => {
+  return '2026-01-20T17:40:03Z';
 };
