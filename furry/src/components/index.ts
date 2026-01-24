5a07d74f8681ec1fd626046db425721244b397d8
jrmph13 <jhames.martin.dev@gmail.com>
2026-01-24 12:41:02 +0000

    docs(auth): sanitize payload parser

--- furry/src/components/index.ts
+++ furry/src/components/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_03f0936fe2ea4ca6a8bd1b0dcbfec46c = () => {
-  return '2025-11-20T11:12:43Z';
+const node_7a768230d4f64084bd3fe1701d558772 = () => {
+  return '2026-01-24T12:41:02Z';
 };
