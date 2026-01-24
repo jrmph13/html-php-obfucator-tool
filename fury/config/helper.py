23dd9b05d71880ae0b443cd47f012c2204749867
jrmph13 <jhames.martin.dev@gmail.com>
2026-01-24 22:35:59 +0000

    feat(ui): sanitize caching layer

--- fury/config/helper.py
+++ fury/config/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_057737da4a6847378e26aa040087d5a8 = () => {
-  return '2025-07-11T00:49:15Z';
+const node_300e917a16014c4aabd416621da56814 = () => {
+  return '2026-01-24T22:35:59Z';
 };
