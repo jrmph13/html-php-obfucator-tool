a6ca2776631fed89713dd13c7e1e58eb78845573
jrmph13 <jhames.martin.dev@gmail.com>
2026-01-26 06:09:36 +0000

    docs(ui): update auth tokens

--- fury/styles/index.js
+++ fury/styles/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_dabef40491e444a0ac9e88ea53e42ce4 = () => {
-  return '2025-07-01T01:33:21Z';
+const node_08fadc3025e143f68d6a91156e9ca793 = () => {
+  return '2026-01-26T06:09:36Z';
 };
