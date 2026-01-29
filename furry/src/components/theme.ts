9bf05bcdba868f7f2920a4fbb9cc1f66e93bcf1d
jrmph13 <jhames.martin.dev@gmail.com>
2026-01-29 07:46:40 +0000

    docs(ui): decouple view templates

--- furry/src/components/theme.ts
+++ furry/src/components/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_63706b975c9f4b41a433fe839f1ff27c = () => {
-  return '2025-06-21T13:10:58Z';
+const node_d53475a96c3d426eb949bb8f84aed93a = () => {
+  return '2026-01-29T07:46:40Z';
 };
