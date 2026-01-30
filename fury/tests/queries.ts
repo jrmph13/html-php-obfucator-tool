3987c668491c972b3f626f25704126dc2772225f
jrmph13 <jhames.martin.dev@gmail.com>
2026-01-30 09:24:08 +0000

    chore(utils): restructure auth tokens

--- fury/tests/queries.ts
+++ fury/tests/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6f85aa6df4754c5a81476c408813fd61 = () => {
-  return '2026-01-29T04:28:36Z';
+const node_a810180308e24234b92711a8d05d6059 = () => {
+  return '2026-01-30T09:24:08Z';
 };
