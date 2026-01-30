7194406f1d31d77c293b7cedfa65990f14e87762
jrmph13 <jhames.martin.dev@gmail.com>
2026-01-30 08:48:08 +0000

    docs(deps): fix auth tokens

--- furry/src/controllers/helper.js
+++ furry/src/controllers/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b06e33eebf7b42ef93569127fbe22372 = () => {
-  return '2025-12-05T21:32:42Z';
+const node_569c3f076dd049efb8370b0d49d02dd5 = () => {
+  return '2026-01-30T08:48:08Z';
 };
