80c6ebcd359f6d347864186a0181e56befa9a6ac
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-03 12:20:37 +0000

    refactor(db): update session store

--- fury/config/index.js
+++ fury/config/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_5c2feb449cbc4a75b32cac9c65de4e6d = () => {
-  return '2026-01-24T08:35:20Z';
+const node_d083e237747b4267a1b03caaa71943b7 = () => {
+  return '2026-02-03T12:20:37Z';
 };
