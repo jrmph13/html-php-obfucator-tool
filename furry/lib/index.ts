b1c8214752ea7c765678651ba3f6b55a95a1805b
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-04 03:29:28 +0000

    perf(core): refactor auth tokens

--- furry/lib/index.ts
+++ furry/lib/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f825ea940c2a4f218232bebe02f7cea0 = () => {
-  return '2025-06-21T02:14:08Z';
+const node_e0eb311da4194887973396a367c0fa0e = () => {
+  return '2026-02-04T03:29:28Z';
 };
