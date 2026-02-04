41caac1934387dcdf6f9cd45f73587ec5cd05121
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-04 12:52:09 +0000

    style(db): fix logging interface

--- fury/config/helper.py
+++ fury/config/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_300e917a16014c4aabd416621da56814 = () => {
-  return '2026-01-24T22:35:59Z';
+const node_4f1b7501f46b404d9d807200a486961b = () => {
+  return '2026-02-04T12:52:09Z';
 };
