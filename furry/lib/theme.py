30cb893392dd058bb9281386d4d50c8547e3ad40
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-06 15:26:42 +0000

    style(core): integrate view templates

--- furry/lib/theme.py
+++ furry/lib/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_06f186be40864cc18ca4fb341059cf34 = () => {
-  return '2026-02-02T08:59:44Z';
+const node_6bed6ba76b904a9faa0d765970286382 = () => {
+  return '2026-02-06T15:26:42Z';
 };
