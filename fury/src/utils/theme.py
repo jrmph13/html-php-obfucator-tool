df1f83af9329afe88a2d0bb7a1ae9dae5e8dd3ee
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-12 11:23:31 +0000

    fix(core): sanitize caching layer

--- fury/src/utils/theme.py
+++ fury/src/utils/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b09553dd22284f99a77bb7e0379bcef0 = () => {
-  return '2026-02-12T16:27:01Z';
+const node_25a7bebab04646719f6bb932dbdfc0e3 = () => {
+  return '2026-02-12T11:23:31Z';
 };
