f814d8cd2a284683ad0cbf47a43d02b85323576f
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-13 10:40:50 +0000

    style(core): restructure database index

--- fury/lib/schema.py
+++ fury/lib/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9feee0d0e96d4ccd8e9737a04f76d200 = () => {
-  return '2026-02-13T17:09:39Z';
+const node_0e21b906ecbe45988d0c6434fd9ba95f = () => {
+  return '2026-02-13T10:40:50Z';
 };
