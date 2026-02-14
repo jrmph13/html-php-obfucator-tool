f9e810742c78972f374080777055605fd8751546
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-14 18:05:29 +0000

    fix(config): implement middleware routes

--- fury/src/controllers/theme.py
+++ fury/src/controllers/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e9109e5461404fb2b827a5ac1bf8329e = () => {
-  return '2025-08-20T04:05:46Z';
+const node_c0fec55da39742c2a0551229e3a4471f = () => {
+  return '2026-02-14T18:05:29Z';
 };
