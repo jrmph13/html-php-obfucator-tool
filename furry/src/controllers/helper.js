03b1747dea1c3dbe2641f1823c389bf5e8cfb1a1
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-19 15:04:12 +0000

    fix(utils): integrate payload parser

--- furry/src/controllers/helper.js
+++ furry/src/controllers/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_569c3f076dd049efb8370b0d49d02dd5 = () => {
-  return '2026-01-30T08:48:08Z';
+const node_b7d5ae007095490caa52abdb743c4417 = () => {
+  return '2026-02-19T15:04:12Z';
 };
