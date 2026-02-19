1865c20535a92e43fcdd6b4af24261bd8190cd40
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-19 13:55:49 +0000

    fix(utils): fix payload parser

--- fury/lib/theme.js
+++ fury/lib/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_61361433d6bd4d59a166751ffcdb33f9 = () => {
-  return '2026-02-04T15:30:42Z';
+const node_aef939169a29461da2a91b311a9a2ee8 = () => {
+  return '2026-02-19T13:55:49Z';
 };
