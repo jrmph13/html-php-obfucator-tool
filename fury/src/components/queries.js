48c8d281cdc0dc4ba631e20a822b52271f1a8d08
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-22 11:01:25 +0000

    chore(api): integrate database index

--- fury/src/components/queries.js
+++ fury/src/components/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9e7b046473804801aa86d2a5800112e0 = () => {
-  return '2025-06-27T13:47:56Z';
+const node_c4fcd5043080441db49588512022cc26 = () => {
+  return '2026-02-22T11:01:25Z';
 };
