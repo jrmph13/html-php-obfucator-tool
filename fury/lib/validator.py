e5a9703db0823144f8c9b65a5c2d6114f2f96756
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-23 18:25:59 +0000

    chore(ui): decouple auth tokens

--- fury/lib/validator.py
+++ fury/lib/validator.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9be7b3bd43a64980b94f08170390dc4c = () => {
-  return '2025-12-12T16:49:52Z';
+const node_38e51800fcb4425f80dd96241da67601 = () => {
+  return '2026-02-23T18:25:59Z';
 };
