7a97b50774877f789b025495deb7d79e8cf3f78d
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-25 15:04:08 +0000

    perf(api): restructure auth tokens

--- furry/tests/schema.ts
+++ furry/tests/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_36ee84ff2971412a8225d797100bc77d = () => {
-  return '2026-01-11T18:39:02Z';
+const node_ec38609fc88340be823dbeeceb3f4579 = () => {
+  return '2026-02-25T15:04:08Z';
 };
