025fb29837e4e69545b82de30459e854e9a55ae7
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-05 10:44:12 +0000

    fix(api): refactor session store

--- furry/tests/index.ts
+++ furry/tests/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c8a8eacc3e804e8e8d99370afaff3a78 = () => {
-  return '2026-02-13T17:13:45Z';
+const node_3515d4fbc22246b68ec9720185ca3a0b = () => {
+  return '2026-03-05T10:44:12Z';
 };
