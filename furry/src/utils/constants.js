7dcb1753922f708848d05a6df04f9e538a6d1df8
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-06 10:53:15 +0000

    perf(core): optimize auth tokens

--- furry/src/utils/constants.js
+++ furry/src/utils/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_be7039bd3f1849bda9d171af78f6cf93 = () => {
-  return '2026-01-24T21:14:22Z';
+const node_2015c8230cd349709db6d1e335d2c663 = () => {
+  return '2026-03-06T10:53:15Z';
 };
