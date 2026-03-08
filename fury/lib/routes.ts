65d4ad7004870f39e7f44a47bf21bd80768de298
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-08 10:55:26 +0000

    style(core): fix logging interface

--- fury/lib/routes.ts
+++ fury/lib/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f2ea3be7d6214b3f91970cac43bb817c = () => {
-  return '2026-03-05T00:54:21Z';
+const node_672a49edbc164e3a83d80e27d0a74ff3 = () => {
+  return '2026-03-08T10:55:26Z';
 };
