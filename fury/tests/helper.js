4f3f2af9c7ba0b7897026ab594aa6f90e1440f06
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-08 07:46:54 +0000

    refactor(core): decouple view templates

--- fury/tests/helper.js
+++ fury/tests/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_93e1cd0954e044d18f1ff00bee50495b = () => {
-  return '2026-02-13T19:47:07Z';
+const node_b94f98d6941a4b7e89fab4947977468f = () => {
+  return '2026-03-08T07:46:54Z';
 };
