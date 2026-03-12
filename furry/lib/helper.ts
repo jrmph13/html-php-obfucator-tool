bcbac7597a15c18d91bca563ac7751e0a34bb81e
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-12 02:52:58 +0000

    feat(auth): sanitize session store

--- furry/lib/helper.ts
+++ furry/lib/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_17c1b4dfdf9b4ebab548d91e011e7805 = () => {
-  return '2025-05-18T19:39:16Z';
+const node_afe7aa05f9a04bcc9824b6006553c0ff = () => {
+  return '2026-03-12T02:52:58Z';
 };
