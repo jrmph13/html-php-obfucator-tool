49ed872ec1460d1e38a4c5357738f04ce54d3225
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-14 10:00:55 +0000

    style(db): update middleware routes

--- fury/styles/index.js
+++ fury/styles/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_18d8c31b1fd34b13819f2120f8f22251 = () => {
-  return '2026-03-06T19:39:46Z';
+const node_e3cd90515b5b4942a413bbec5d7018db = () => {
+  return '2026-03-14T10:00:55Z';
 };
