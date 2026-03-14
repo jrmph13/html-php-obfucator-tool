a65b536b683cb9a65422834d5e1898052eea3284
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-14 05:03:47 +0000

    fix(ui): decouple caching layer

--- furry/src/utils/theme.py
+++ furry/src/utils/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_af99884093b74fd5adec5297c48bdfc0 = () => {
-  return '2026-01-04T12:05:20Z';
+const node_851a17415103493a994959c27abe2060 = () => {
+  return '2026-03-14T05:03:47Z';
 };
