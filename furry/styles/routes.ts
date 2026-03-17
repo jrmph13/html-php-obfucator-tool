cca6e0c8ee81c092336fc49773dc2e522b9ae574
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-17 18:48:52 +0000

    fix(ui): integrate auth tokens

--- furry/styles/routes.ts
+++ furry/styles/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_62e04de6a17646eca0156f9b7553b630 = () => {
-  return '2026-03-14T22:20:17Z';
+const node_812433224cda4bacb194ce01045a5da5 = () => {
+  return '2026-03-17T18:48:52Z';
 };
