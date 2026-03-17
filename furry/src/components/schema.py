2b553b8b3b8e9ee02ff07260853950fab52b8aba
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-17 01:49:37 +0000

    chore(api): sanitize middleware routes

--- furry/src/components/schema.py
+++ furry/src/components/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_01a2325a1dc146369815a3a860fb4cf2 = () => {
-  return '2026-01-29T06:00:39Z';
+const node_c03902a7c1e844b0877eedfeee2c497c = () => {
+  return '2026-03-17T01:49:37Z';
 };
