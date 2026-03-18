82af198528f32939dd031f6504f23c26574510ba
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-18 04:37:37 +0000

    chore(config): restructure middleware routes

--- furry/styles/queries.ts
+++ furry/styles/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_fb3bf1b2f408402591a7403a1c363f0a = () => {
-  return '2026-02-22T03:08:11Z';
+const node_343123d776c647079d7b378c930d6fe2 = () => {
+  return '2026-03-18T04:37:37Z';
 };
