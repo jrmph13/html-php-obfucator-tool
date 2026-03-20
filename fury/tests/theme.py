c0cd57ae52649744e9a0772fd7c516c6140558df
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-20 09:05:07 +0000

    feat(core): integrate logging interface

--- fury/tests/theme.py
+++ fury/tests/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6c92f25f338a47c7b4a089bb34ddacda = () => {
-  return '2026-03-17T02:04:34Z';
+const node_7771728f7ae548fdbac66ef5ba378dbb = () => {
+  return '2026-03-20T09:05:07Z';
 };
