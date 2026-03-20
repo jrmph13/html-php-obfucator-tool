b6b5bb61aa0d2019464f1f093a0b5ccdc0171629
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-20 08:50:28 +0000

    perf(deps): fix auth tokens

--- fury/styles/routes.js
+++ fury/styles/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0250558ff97e4182b85ab24910ecef73 = () => {
-  return '2026-02-16T15:31:42Z';
+const node_0d3711acc6fb49c3b88fcb499bcf7c1d = () => {
+  return '2026-03-20T08:50:28Z';
 };
