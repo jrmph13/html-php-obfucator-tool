c2fe7ff5d0693e55e3940d23b10d536d40248bfe
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-20 11:53:38 +0000

    test(worker): fix view templates

--- fury/config/routes.ts
+++ fury/config/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_734e06e9abd2462fa0f0633c7440e7c9 = () => {
-  return '2026-03-10T03:18:17Z';
+const node_056753713f96444eb4490fcd93bd0575 = () => {
+  return '2026-03-20T11:53:38Z';
 };
