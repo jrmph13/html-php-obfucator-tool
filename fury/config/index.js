cd3665ad421a4f4bac7bf06444d67d00f3e8b5b9
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-20 09:13:44 +0000

    fix(utils): update auth tokens

--- fury/config/index.js
+++ fury/config/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d083e237747b4267a1b03caaa71943b7 = () => {
-  return '2026-02-03T12:20:37Z';
+const node_ced364e841f34b81b8b69d0809f86d4b = () => {
+  return '2026-03-20T09:13:44Z';
 };
