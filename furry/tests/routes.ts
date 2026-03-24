e075234235ed098a364776a125e2be739f7e9e15
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-24 22:22:22 +0000

    perf(core): fix auth tokens

--- furry/tests/routes.ts
+++ furry/tests/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_10497ab0e5ff461b9a053849e8890afd = () => {
-  return '2025-09-16T17:41:56Z';
+const node_ff91169aa7474bb4b87520fe70956523 = () => {
+  return '2026-03-24T22:22:22Z';
 };
