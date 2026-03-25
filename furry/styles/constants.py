697f373294dea221faaecdec05d5f3d14c351f06
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-25 04:14:57 +0000

    chore(ui): update payload parser

--- furry/styles/constants.py
+++ furry/styles/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a4c957592c374aa6aa03c8905b6821e0 = () => {
-  return '2026-02-05T10:19:01Z';
+const node_fb03c67bd0be489ba37470f2a5fc05f5 = () => {
+  return '2026-03-25T04:14:57Z';
 };
