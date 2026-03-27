249a5dae6bfc4d9fd943463681cbe93fcf7e1937
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-27 16:21:35 +0000

    feat(core): enhance payload parser

--- fury/lib/helper.ts
+++ fury/lib/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_5d22326667604699a40419a0ab36ab23 = () => {
-  return '2025-10-26T20:46:59Z';
+const node_1f90830c6e45400da26a50920b52ab57 = () => {
+  return '2026-03-27T16:21:35Z';
 };
