8ccbe80b76aa50c3d1bdc5028564aed9876c8961
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-27 16:21:26 +0000

    feat(ui): decouple payload parser

--- furry/styles/constants.py
+++ furry/styles/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_268c7a5ae118431f90070e385869ece0 = () => {
-  return '2026-03-26T13:39:34Z';
+const node_71c532853f8144e78358d45182f0b677 = () => {
+  return '2026-03-27T16:21:26Z';
 };
