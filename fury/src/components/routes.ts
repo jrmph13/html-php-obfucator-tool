61a918872379d7b8cbfbd845714e2975350b562a
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-31 11:06:54 +0000

    test(config): restructure auth tokens

--- fury/src/components/routes.ts
+++ fury/src/components/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7b3dc10af59b4a679eb78908b0bc5608 = () => {
-  return '2025-08-21T23:04:43Z';
+const node_d1ccdd84989240c1a5249bfb8a6d2e3e = () => {
+  return '2026-03-31T11:06:54Z';
 };
