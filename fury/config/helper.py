81a21b88a78dc0961fcf9787cbc052aa958e88c2
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-01 16:56:30 +0000

    test(config): decouple auth tokens

--- fury/config/helper.py
+++ fury/config/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_da529a6616954330a7c9b5997104ea7a = () => {
-  return '2026-02-23T11:55:51Z';
+const node_833171778ce54bc381b3f26837fd7374 = () => {
+  return '2026-04-01T16:56:30Z';
 };
