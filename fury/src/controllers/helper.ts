7517554cf8fab1a2a8e99a1281ecfc931cb4e2e5
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-02 05:46:30 +0000

    perf(config): integrate middleware routes

--- fury/src/controllers/helper.ts
+++ fury/src/controllers/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0ebea6db225e4732ac4e2379c1b5151c = () => {
-  return '2026-02-14T16:12:04Z';
+const node_48e2b675cc884e438c6b4c7a3e29021a = () => {
+  return '2026-04-02T05:46:30Z';
 };
