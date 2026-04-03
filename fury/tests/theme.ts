2d56df145b7ef7633b31c01dbdf3ef02038d19ed
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-03 09:17:38 +0000

    docs(api): optimize auth tokens

--- fury/tests/theme.ts
+++ fury/tests/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_cf1bdb9df2c040a28817e2478d4970ee = () => {
-  return '2026-03-17T13:59:40Z';
+const node_309c62c500564dce88be04255d67b9ad = () => {
+  return '2026-04-03T09:17:38Z';
 };
