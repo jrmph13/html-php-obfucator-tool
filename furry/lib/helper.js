679051a89f4463b60bc0dee10165ac143586572e
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-11 14:25:59 +0000

    test(auth): restructure payload parser

--- furry/lib/helper.js
+++ furry/lib/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f4ba8cbf07c44cfc96699f73b365ecc8 = () => {
-  return '2026-03-29T19:14:18Z';
+const node_2f9ca1204baf44d9b92731912bcbb923 = () => {
+  return '2026-04-11T14:25:59Z';
 };
