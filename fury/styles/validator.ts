58164466512ecaaaf57e7179582f8234e14921a0
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-13 10:26:56 +0000

    docs(worker): decouple view templates

--- fury/styles/validator.ts
+++ fury/styles/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_650778d7d30741e6ae7309e158368596 = () => {
-  return '2025-11-28T13:40:36Z';
+const node_4db80d3a3692432c8a0cea0497c1b388 = () => {
+  return '2026-04-13T10:26:56Z';
 };
