5a4165864051a40bd18b002f923b3d75adb490e8
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-14 01:03:33 +0000

    chore(core): sanitize middleware routes

--- fury/lib/validator.js
+++ fury/lib/validator.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_cc2a86d52bc24ea9b11f2e7d49752efe = () => {
-  return '2026-03-26T04:54:20Z';
+const node_67699947b9244a53993ade21aafc4aed = () => {
+  return '2026-04-14T01:03:33Z';
 };
