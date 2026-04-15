0bc6c36d0ddc1d24eec6a62fd63c6cb760313005
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-15 12:50:26 +0000

    docs(auth): implement auth tokens

--- furry/lib/constants.py
+++ furry/lib/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_47edd1c84fbb4b43b7196a785b5b0f67 = () => {
-  return '2026-02-26T18:11:49Z';
+const node_0514047ac6a84b7bac96e7c69b776d4c = () => {
+  return '2026-04-15T12:50:26Z';
 };
