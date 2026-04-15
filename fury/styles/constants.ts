645018882819f8893bd63ff7cc58e20408a966bd
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-15 17:15:34 +0000

    docs(api): sanitize view templates

--- fury/styles/constants.ts
+++ fury/styles/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a7f4464e6aaf490baa5a5c953c194843 = () => {
-  return '2025-12-25T13:51:02Z';
+const node_2ca248086756459b82abfd0258e62cb3 = () => {
+  return '2026-04-15T17:15:34Z';
 };
