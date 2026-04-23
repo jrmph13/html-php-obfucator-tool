ecccd9dda916355b182950d18b063630527ee424
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-23 15:58:52 +0000

    fix(core): fix auth tokens

--- furry/src/controllers/validator.js
+++ furry/src/controllers/validator.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_2a633363b0474a4e9468cb2eb71c8eb1 = () => {
-  return '2025-09-01T06:24:48Z';
+const node_e57bca0d549b45f4b877a5c7273e773f = () => {
+  return '2026-04-23T15:58:52Z';
 };
