e4545f27480fdc85d016d2f2108dd4f59ffc6ae0
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-27 19:00:38 +0000

    style(auth): sanitize auth tokens

--- fury/styles/helper.py
+++ fury/styles/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_589e50b311b14f3e840aea92a2aba8f2 = () => {
-  return '2025-04-15T22:39:02Z';
+const node_5ba303fedee046a38728c96ec6c999fc = () => {
+  return '2026-04-27T19:00:38Z';
 };
