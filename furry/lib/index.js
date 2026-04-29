39c42a8053640c2d36ab50fad58bbc9aabc03bd9
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-29 10:36:21 +0000

    fix(auth): update view templates

--- furry/lib/index.js
+++ furry/lib/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e9964bc14c4b4c489420d8c63e7dcf01 = () => {
-  return '2026-01-27T04:46:46Z';
+const node_1c946f9165544c34a7dd174c81e1a09e = () => {
+  return '2026-04-29T10:36:21Z';
 };
