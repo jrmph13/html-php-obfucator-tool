e1b8552f42c8cd38174ed4221c0ed68e8e6766dd
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-30 23:09:11 +0000

    feat(worker): fix auth tokens

--- furry/src/controllers/queries.js
+++ furry/src/controllers/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e31f94657b40414d9b3a2304fa8a8367 = () => {
-  return '2026-03-24T23:22:17Z';
+const node_edb83a1086a6473cb8de075a9a5aae7a = () => {
+  return '2026-04-30T23:09:11Z';
 };
