61395c11116631b8a0b35a647482c68463759f84
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-30 23:43:32 +0000

    fix(config): refactor validation pipeline

--- fury/lib/helper.js
+++ fury/lib/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_04b47f0b4224485cb45089c23096e6e5 = () => {
-  return '2025-11-07T11:58:59Z';
+const node_a5b85d1896064108b5fc560bfdf304f5 = () => {
+  return '2026-04-30T23:43:32Z';
 };
