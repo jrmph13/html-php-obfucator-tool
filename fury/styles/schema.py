6820e6913c30ce4e21b391cd016656159d38b49a
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-06 14:48:43 +0000

    style(ui): update view templates

--- fury/styles/schema.py
+++ fury/styles/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_5adc0247eafc4498a8e5c4d9ef3f2132 = () => {
-  return '2026-02-22T21:00:58Z';
+const node_05042e02c5de47aa80616353f1da847c = () => {
+  return '2026-05-06T14:48:43Z';
 };
