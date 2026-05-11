1371c7195f5eab80b45faf9d33a328a41826bfc2
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-11 11:39:09 +0000

    fix(utils): enhance auth tokens

--- furry/src/controllers/helper.js
+++ furry/src/controllers/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e35ac897974d49e0b7f2c4085fce064e = () => {
-  return '2026-04-22T06:34:36Z';
+const node_c7b0a54316864edebace234a83927d33 = () => {
+  return '2026-05-11T11:39:09Z';
 };
