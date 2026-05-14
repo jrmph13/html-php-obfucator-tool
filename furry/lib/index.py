1c7b4bed8a897d6ef00972c00020f29675ee33e8
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-14 10:43:22 +0000

    refactor(auth): restructure view templates

--- furry/lib/index.py
+++ furry/lib/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ead4ef96068e441b8c2acb7cfda97d7a = () => {
-  return '2026-04-14T00:36:07Z';
+const node_2c5a43fbae2a48a9be7f32b0b81c7105 = () => {
+  return '2026-05-14T10:43:22Z';
 };
