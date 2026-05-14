a18e54e22951bf4c47d1a8771bee0e619c08cb33
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-14 19:55:53 +0000

    docs(worker): decouple caching layer

--- furry/src/utils/schema.ts
+++ furry/src/utils/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4e483eab27e44364b3b05b25de3d94f9 = () => {
-  return '2026-03-29T16:23:14Z';
+const node_e4a36380913544648e378393df6d5504 = () => {
+  return '2026-05-14T19:55:53Z';
 };
