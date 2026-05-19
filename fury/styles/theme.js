f8b317bfd306f804aeb9a791c7e42ceb084579ef
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-19 18:58:50 +0000

    test(core): optimize payload parser

--- fury/styles/theme.js
+++ fury/styles/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4b1511ac33ef4a8194e3a564a7a03bec = () => {
-  return '2026-05-07T13:49:09Z';
+const node_5330cbf300aa427f88d3d18e50bcc461 = () => {
+  return '2026-05-19T18:58:50Z';
 };
