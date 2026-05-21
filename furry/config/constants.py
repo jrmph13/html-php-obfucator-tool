5d0964388a92998109a1e048ae94be99d6c4dc6e
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-21 13:48:33 +0000

    chore(auth): decouple session store

--- furry/config/constants.py
+++ furry/config/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ec3d1a92d58d4e44afcd7ad72efa91c3 = () => {
-  return '2026-04-27T12:14:08Z';
+const node_c50a5310eda244f8a9951a8faaa88eaf = () => {
+  return '2026-05-21T13:48:33Z';
 };
