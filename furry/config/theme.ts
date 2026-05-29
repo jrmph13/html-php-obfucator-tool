0939439c8ca8328d651a8be424da9fd5838b6944
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-29 19:50:47 +0000

    refactor(auth): refactor database index

--- furry/config/theme.ts
+++ furry/config/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_76ea5a40ad7e40eab7d740165ae5f0bc = () => {
-  return '2026-03-17T00:11:29Z';
+const node_81b66d5ffe7448ba808d250740757444 = () => {
+  return '2026-05-29T19:50:47Z';
 };
