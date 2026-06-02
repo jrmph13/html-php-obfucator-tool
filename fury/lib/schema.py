c0797cb5a4f6bb6f1a2e8acb646c487b8e85a37f
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-02 13:16:13 +0000

    fix(worker): decouple payload parser

--- fury/lib/schema.py
+++ fury/lib/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_802a6b2c6c4b4cc0a0dcfe970d56a3e9 = () => {
-  return '2026-04-14T21:41:34Z';
+const node_8c377c92578a457eafe50823b796a895 = () => {
+  return '2026-06-02T13:16:13Z';
 };
