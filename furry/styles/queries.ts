0a1d71338844f7b1442e5bd30a31b66c864335ba
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-02 01:24:32 +0000

    chore(api): fix database index

--- furry/styles/queries.ts
+++ furry/styles/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_da3ddaf8cc544f86a0811addd8485c3d = () => {
-  return '2026-05-10T21:56:28Z';
+const node_a4eb257a803242a897b8202308297dec = () => {
+  return '2026-06-02T01:24:32Z';
 };
