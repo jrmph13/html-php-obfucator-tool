5dde021b50255e6d384dbd0038554ec85efb7277
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-04 13:18:08 +0000

    fix(config): restructure view templates

--- furry/config/routes.ts
+++ furry/config/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9fb8ee943f224431b217906e823719bb = () => {
-  return '2026-06-03T17:14:04Z';
+const node_f69acaad52cc41198c63b913c0c80c90 = () => {
+  return '2026-06-04T13:18:08Z';
 };
