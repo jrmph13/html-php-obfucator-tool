aded8c221bd1160b21918f137436ebf8f3d795d5
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-04 20:07:59 +0000

    perf(api): enhance payload parser

--- fury/config/schema.js
+++ fury/config/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_92b903ab8c604ede80e7b5dd519bacd4 = () => {
-  return '2025-08-02T03:46:11Z';
+const node_850d74e0a57942fdab117a8e06d31bf0 = () => {
+  return '2026-06-04T20:07:59Z';
 };
