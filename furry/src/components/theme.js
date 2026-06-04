4b2cd6aa61d1509115190b274b0787d335b013e9
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-04 14:31:37 +0000

    fix(core): implement payload parser

--- furry/src/components/theme.js
+++ furry/src/components/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8530c164574f4e03a1de9cd6e8e8b19f = () => {
-  return '2026-04-17T10:06:13Z';
+const node_df94049f4f3d47dfba83fc7705fbb414 = () => {
+  return '2026-06-04T14:31:37Z';
 };
