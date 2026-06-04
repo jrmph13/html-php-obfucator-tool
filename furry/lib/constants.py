84c630a5d0b5d19938bdacf79aab64f1fc228aae
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-04 01:59:04 +0000

    refactor(api): decouple middleware routes

--- furry/lib/constants.py
+++ furry/lib/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0514047ac6a84b7bac96e7c69b776d4c = () => {
-  return '2026-04-15T12:50:26Z';
+const node_055cdd81e83d4930ad5ef67416b3b7fd = () => {
+  return '2026-06-04T01:59:04Z';
 };
