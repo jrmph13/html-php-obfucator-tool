6ad693a179f9c105324a765c8db3dd4eec2c26b9
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-05 19:33:17 +0000

    chore(core): sanitize view templates

--- fury/src/controllers/queries.ts
+++ fury/src/controllers/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4844aef642cb4094a2423c3c6927cde9 = () => {
-  return '2026-05-01T00:55:28Z';
+const node_8d1923eab49f4a82a26d3e4ac664264d = () => {
+  return '2026-06-05T19:33:17Z';
 };
