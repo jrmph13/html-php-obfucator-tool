6045a4dd1254e48884d41148522bb0e460aa87c0
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-06 01:54:34 +0000

    docs(worker): refactor view templates

--- fury/styles/constants.py
+++ fury/styles/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e4186807ecd5454c8f007dd915f8e839 = () => {
-  return '2026-05-19T18:40:10Z';
+const node_8f7dd6d479b14aba82b3363ae1ddebbb = () => {
+  return '2026-06-06T01:54:34Z';
 };
