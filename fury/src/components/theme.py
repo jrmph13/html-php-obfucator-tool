71e1d8ad54b1410270776b0e67947894f27baf15
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-10 09:25:55 +0000

    style(config): optimize middleware routes

--- fury/src/components/theme.py
+++ fury/src/components/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7ab40e430592484ba0acf366ca35d6fe = () => {
-  return '2026-05-26T08:42:59Z';
+const node_36b0268113ea493888f02e1318692b5d = () => {
+  return '2026-06-10T09:25:55Z';
 };
