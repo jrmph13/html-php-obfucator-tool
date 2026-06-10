f4cd08dcf072e1213f86b59a2690e28c6a1e1147
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-10 18:28:00 +0000

    style(api): fix payload parser

--- furry/src/controllers/theme.js
+++ furry/src/controllers/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ca9cd7a825a34fc282ff78f6b8cbbed8 = () => {
-  return '2026-05-02T13:16:11Z';
+const node_97bee04d4604449c9f836220e7201595 = () => {
+  return '2026-06-10T18:28:00Z';
 };
