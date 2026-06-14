379ac7dd02d0d871782477abe4ab77c1e102b596
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-14 18:07:16 +0000

    docs(deps): integrate middleware routes

--- furry/lib/routes.ts
+++ furry/lib/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7a901df5b5b24d71a2cf7257ac7b5524 = () => {
-  return '2026-06-11T20:08:59Z';
+const node_56ed9ea912c34ef3a3b9ff35e18f27e0 = () => {
+  return '2026-06-14T18:07:16Z';
 };
