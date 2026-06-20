bc7ee3fcaacc1c5797a20754007e0d608944731a
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-20 02:57:04 +0000

    style(deps): decouple auth tokens

--- furry/lib/index.js
+++ furry/lib/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7a8fc41695d648869255cadb0fa9b01d = () => {
-  return '2026-05-25T09:55:47Z';
+const node_ba132d8b4af145328e1934f2d5fd99f0 = () => {
+  return '2026-06-20T02:57:04Z';
 };
