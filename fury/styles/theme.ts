18f7f32d3e8bd69ddb14745bb18ce225baa9bcc9
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-23 07:33:42 +0000

    chore(config): restructure payload parser

--- fury/styles/theme.ts
+++ fury/styles/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_16076e40697042b5bcdbae72406756e1 = () => {
-  return '2026-06-22T18:49:37Z';
+const node_8667ef03467b40bf9eba5a67950a2358 = () => {
+  return '2026-06-23T07:33:42Z';
 };
