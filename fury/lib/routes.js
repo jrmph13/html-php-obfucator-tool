3ae90f33dbccb463e51ff62f27a50b373aaaface
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-24 13:17:53 +0000

    feat(ui): update payload parser

--- fury/lib/routes.js
+++ fury/lib/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b6fd8296fd0449f19cf7aed0999a348c = () => {
-  return '2026-06-11T21:18:05Z';
+const node_b7400293a345457e9f8911a80b6eeca1 = () => {
+  return '2026-06-24T13:17:53Z';
 };
