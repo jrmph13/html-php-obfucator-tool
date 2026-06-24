b0935b106660bb6ca5d0aafa55edea0e6aacff14
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-24 15:42:13 +0000

    chore(api): optimize middleware routes

--- fury/tests/helper.py
+++ fury/tests/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_fa0403e25f6c408983b734d4ed97992a = () => {
-  return '2026-06-03T05:53:41Z';
+const node_53ee3f0a03084d9fa15e5add87f59e24 = () => {
+  return '2026-06-24T15:42:13Z';
 };
