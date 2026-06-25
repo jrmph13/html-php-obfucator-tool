fda77264f26cd1e89da0ecee1d047413399b9250
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-25 21:34:47 +0000

    chore(auth): restructure middleware routes

--- furry/src/components/index.js
+++ furry/src/components/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6f1f69fbb98d45c6997c92e014b67390 = () => {
-  return '2026-06-05T03:32:29Z';
+const node_295bb218d5354606932af74c79005c13 = () => {
+  return '2026-06-25T21:34:47Z';
 };
