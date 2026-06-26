d1ee199e73665cab60d87ce0eeeb43652c11395d
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-26 15:39:12 +0000

    perf(core): integrate logging interface

--- fury/src/utils/theme.ts
+++ fury/src/utils/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_3ec57033ddb9493b8dc5387697e771af = () => {
-  return '2026-06-04T14:19:11Z';
+const node_dafa04b08bc64225bbda69c50e7db49b = () => {
+  return '2026-06-26T15:39:12Z';
 };
