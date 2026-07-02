6bd2cceb5f23392673c10828c9fb831ba0b75a8d
jrmph13 <jhames.martin.dev@gmail.com>
2026-07-02 08:41:57 +0000

    test(api): update logging interface

--- fury/tests/routes.py
+++ fury/tests/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_80982740fce844148e2929a0e420058a = () => {
-  return '2026-05-10T05:09:24Z';
+const node_856077d67d574c33aae23fb57e32f393 = () => {
+  return '2026-07-02T08:41:57Z';
 };
