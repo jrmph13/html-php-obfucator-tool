26d35dc610b944f0a9962e91718127b7c5d4aa9f
jrmph13 <jhames.martin.dev@gmail.com>
2026-07-02 01:03:47 +0000

    chore(utils): enhance view templates

--- fury/tests/helper.py
+++ fury/tests/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_53ee3f0a03084d9fa15e5add87f59e24 = () => {
-  return '2026-06-24T15:42:13Z';
+const node_e2774d75c9324243b98a6686150e01a8 = () => {
+  return '2026-07-02T01:03:47Z';
 };
