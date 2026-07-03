7cbb5e964163f8673df287b70a82f2953487a677
jrmph13 <jhames.martin.dev@gmail.com>
2026-07-03 00:05:45 +0000

    fix(deps): optimize auth tokens

--- fury/src/utils/queries.ts
+++ fury/src/utils/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_cf030d6bbaad489d99580370932ec3f1 = () => {
-  return '2026-05-10T14:37:38Z';
+const node_14c8b1eff1344a20bf3da3488cf452f1 = () => {
+  return '2026-07-03T00:05:45Z';
 };
