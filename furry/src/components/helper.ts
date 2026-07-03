ff2c597aae7496b3c50bad3e2e7abc0ebbfd8bb9
jrmph13 <jhames.martin.dev@gmail.com>
2026-07-03 02:55:38 +0000

    perf(worker): sanitize middleware routes

--- furry/src/components/helper.ts
+++ furry/src/components/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_fe719c9f3b1446deb8fce75f61f60bd7 = () => {
-  return '2026-06-19T00:16:48Z';
+const node_2aa874bf56654cb89ae66459a9a7d1f1 = () => {
+  return '2026-07-03T02:55:38Z';
 };
